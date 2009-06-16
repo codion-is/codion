d7a6281a1bac530c25095d6528368de103d64fa6
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2009-06-16 22:48:35 +0000

    git-svn-id: svn://svn.hafro.is/jminor/trunk@5466 51816742-9f83-410d-85ed-e7bcf7042953

---
+++ resources/demos/petstore/petstore_client_h2_embedded.bat
@@ -0,0 +1,1 @@
+java -Djminor.client.connection.type=local -Djminor.db.type=h2_embedded -Djminor.db.host=h2db/h2 -Djava.security.policy=jminor_demos.policy -Xmx128m -cp lib/jminor-client.jar;lib/jminor-demos.jar;lib/jminor-common.jar;lib/log4j-1.2.15.jar;lib/jcalendar-1.3.2.jar;lib/jasperreports-3.0.0.jar;lib/h2-1.1.114.jar org.jminor.framework.demos.petstore.client.ui.PetstoreAppPanel
