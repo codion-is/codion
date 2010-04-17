ca81e1116c62f9c6933f3a6100081110473d1183
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2010-04-17 14:43:10 +0000

    git-svn-id: svn://svn.hafro.is/jminor/trunk@5944 51816742-9f83-410d-85ed-e7bcf7042953

---
+++ srctest/org/jminor/framework/db/provider/EntityDbProviderFactoryTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2004 - 2010, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package org.jminor.framework.db.provider;
+
+import org.jminor.common.model.User;
+import org.jminor.framework.Configuration;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * User: Björn Darri
+ * Date: 17.4.2010
+ * Time: 14:30:18
+ */
+public class EntityDbProviderFactoryTest {
+
+  @Test
+  public void test() throws Exception {
+    final String connectionType = System.getProperty(Configuration.CLIENT_CONNECTION_TYPE);
+
+    System.setProperty(Configuration.CLIENT_CONNECTION_TYPE, Configuration.CONNECTION_TYPE_LOCAL);
+    EntityDbProvider dbProvider = EntityDbProviderFactory.createEntityDbProvider(User.UNIT_TEST_USER, "test");
+    assertTrue(dbProvider instanceof EntityDbLocalProvider);
+
+    System.setProperty(Configuration.CLIENT_CONNECTION_TYPE, Configuration.CONNECTION_TYPE_REMOTE);
+    dbProvider = EntityDbProviderFactory.createEntityDbProvider(User.UNIT_TEST_USER, "test");
+    assertTrue(dbProvider.getClass().getSimpleName().equals("EntityDbRemoteProvider"));
+
+    if (connectionType != null)
+      System.setProperty(Configuration.CLIENT_CONNECTION_TYPE, connectionType);
+  }
+}
