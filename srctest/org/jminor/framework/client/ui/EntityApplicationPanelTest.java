ef8822b6b1eb77fce76efaabf3c0f3496f6f2a15
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2010-07-01 13:36:57 +0000

    git-svn-id: svn://svn.hafro.is/jminor/trunk@6219 51816742-9f83-410d-85ed-e7bcf7042953

--- srctest/org/jminor/framework/client/ui/EntityApplicationPanelTest.java
+++ srctest/org/jminor/framework/client/ui/EntityApplicationPanelTest.java
@@ -5,7 +5,6 @@ package org.jminor.framework.client.ui;
 
 import org.jminor.common.model.CancelException;
 import org.jminor.common.model.User;
-import org.jminor.common.ui.UiUtil;
 import org.jminor.framework.db.EntityDbConnectionTest;
 import org.jminor.framework.demos.empdept.beans.ui.DepartmentPanel;
 import org.jminor.framework.demos.empdept.client.EmpDeptAppModel;
@@ -14,8 +13,6 @@ import org.jminor.framework.demos.empdept.client.ui.EmpDeptAppPanel;
 import org.junit.Test;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import java.awt.GraphicsEnvironment;
 import java.util.Arrays;
 
 public class EntityApplicationPanelTest {
@@ -28,19 +25,8 @@ public class EntityApplicationPanelTest {
         return User.UNIT_TEST_USER;
       }
     };
-    if (GraphicsEnvironment.isHeadless()) {
-      panel.initialize(new EmpDeptAppModel(EntityDbConnectionTest.DB_PROVIDER));
-    }
-    else {
-      panel.startApplication("test", null, false, null);
-    }
-
+    panel.initialize(new EmpDeptAppModel(EntityDbConnectionTest.DB_PROVIDER));
     panel.getEntityPanel(DepartmentPanel.class).getTablePanel().getTableModel().setSelectedItemIndexes(Arrays.asList(0,1,2));
-    if (!GraphicsEnvironment.isHeadless()) {
-      final JFrame frame = UiUtil.getParentFrame(panel);
-      frame.setVisible(false);
-      frame.dispose();
-    }
     panel.getModel().getDbProvider().disconnect();
   }
 }
