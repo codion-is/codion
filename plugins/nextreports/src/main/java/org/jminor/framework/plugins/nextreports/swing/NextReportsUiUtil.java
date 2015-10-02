41ec5923391ceffaabd0bf2fbd7d062a0caeeda8
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2015-10-02 21:29:29 +0000

    git-svn-id: svn://svn.hafro.is/jminor/trunk@7847 51816742-9f83-410d-85ed-e7bcf7042953

--- plugins/nextreports/src/main/java/org/jminor/framework/plugins/nextreports/swing/NextReportsUiUtil.java
+++ plugins/nextreports/src/main/java/org/jminor/framework/plugins/nextreports/swing/NextReportsUiUtil.java
@@ -3,6 +3,7 @@
  */
 package org.jminor.framework.plugins.nextreports.swing;
 
+import org.jminor.common.model.reports.ReportResult;
 import org.jminor.framework.plugins.nextreports.model.NextReportsResult;
 
 import java.awt.Desktop;
@@ -11,7 +12,7 @@ import java.io.IOException;
 
 public final class NextReportsUiUtil {
 
-  public static void openReport(final NextReportsResult reportsResult, final File file) throws IOException {
-    Desktop.getDesktop().open(reportsResult.writeResultToFile(file));
+  public static void openReport(final ReportResult<NextReportsResult> reportsResult, final File file) throws IOException {
+    Desktop.getDesktop().open(reportsResult.getResult().writeResultToFile(file));
   }
 }
