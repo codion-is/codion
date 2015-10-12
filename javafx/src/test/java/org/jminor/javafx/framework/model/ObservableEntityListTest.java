54e8d0c333fe534d0f957aeea0a1d3ae8545aaff
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2015-10-12 17:34:00 +0000

    git-svn-id: svn://svn.hafro.is/jminor/trunk@7870 51816742-9f83-410d-85ed-e7bcf7042953

---
+++ javafx/src/test/java/org/jminor/javafx/framework/model/ObservableEntityListTest.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2004 - 2015, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package org.jminor.javafx.framework.model;
+
+import org.junit.Test;
+
+public final class ObservableEntityListTest {
+
+  @Test
+  public void dummyTest() {}
+}
