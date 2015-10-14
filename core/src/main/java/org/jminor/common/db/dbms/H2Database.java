df23f99813224c63c3cb16ac5d4f3fadebdd7fcd
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2015-10-14 11:23:31 +0000

    H2Database.initializeDatabase() bug fixed, did not work with Windows paths, since \ is an escape character in the INIT clause
    git-svn-id: svn://svn.hafro.is/jminor/trunk@7877 51816742-9f83-410d-85ed-e7bcf7042953

--- core/src/main/java/org/jminor/common/db/dbms/H2Database.java
+++ core/src/main/java/org/jminor/common/db/dbms/H2Database.java
@@ -205,7 +205,7 @@ public final class H2Database extends AbstractDatabase {
       final Properties properties = new Properties();
       properties.put(USER_PROPERTY, SYSADMIN_USERNAME);
       final String url = (inMemory ? URL_PREFIX_MEM : URL_PREFIX_FILE) + databaseName
-              + ";DB_CLOSE_DELAY=-1;INIT=RUNSCRIPT FROM '" + scriptPath + "'";
+              + ";DB_CLOSE_DELAY=-1;INIT=RUNSCRIPT FROM '" + scriptPath.replace("\\", "/") + "'";
       try {
         DriverManager.getConnection(url, properties).close();
       }
