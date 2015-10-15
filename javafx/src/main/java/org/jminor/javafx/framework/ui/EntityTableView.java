4a00ff3221f64ed5c7e212a02283c21f17aa89bb
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2015-10-15 21:32:36 +0000

    git-svn-id: svn://svn.hafro.is/jminor/trunk@7887 51816742-9f83-410d-85ed-e7bcf7042953

--- javafx/src/main/java/org/jminor/javafx/framework/ui/EntityTableView.java
+++ javafx/src/main/java/org/jminor/javafx/framework/ui/EntityTableView.java
@@ -7,6 +7,7 @@ import org.jminor.common.model.Util;
 import org.jminor.framework.domain.Entities;
 import org.jminor.framework.domain.Entity;
 import org.jminor.framework.domain.Property;
+import org.jminor.framework.i18n.FrameworkMessages;
 import org.jminor.javafx.framework.model.ObservableEntityList;
 
 import javafx.collections.transformation.FilteredList;
@@ -17,19 +18,20 @@ import javafx.scene.control.TextField;
 
 public class EntityTableView extends TableView<Entity> {
 
-  private final ObservableEntityList observableList;
+  private final ObservableEntityList entityList;
   private final TextField filterText = new TextField();
 
-  public EntityTableView(final ObservableEntityList observableList) {
-    super(new FilteredList<Entity>(observableList));
-    this.observableList = observableList;
+  public EntityTableView(final ObservableEntityList entityList) {
+    super(new FilteredList<Entity>(entityList));
+    this.entityList = entityList;
+    filterText.setPromptText(FrameworkMessages.get(FrameworkMessages.SEARCH));
     getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     initializeColumns();
     bindEvents();
   }
 
   public ObservableEntityList getEntityList() {
-    return observableList;
+    return entityList;
   }
 
   public TextField getFilterTextField() {
@@ -37,8 +39,8 @@ public class EntityTableView extends TableView<Entity> {
   }
 
   private void initializeColumns() {
-    for (final Property property : Entities.getVisibleProperties(observableList.getEntityID())) {
-      getColumns().add(new EntityTableColumn(property, observableList.getCellValueFactory(property)));
+    for (final Property property : Entities.getVisibleProperties(entityList.getEntityID())) {
+      getColumns().add(new EntityTableColumn(property, entityList.getCellValueFactory(property)));
     }
   }
 
