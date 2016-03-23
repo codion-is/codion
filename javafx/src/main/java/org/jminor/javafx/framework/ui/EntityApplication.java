befd987d08ed1ff91484dff738ffdbc1d817aa44
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2016-03-23 22:26:40 +0000

    JavaFX improvements
    git-svn-id: svn://svn.hafro.is/jminor/trunk@7997 51816742-9f83-410d-85ed-e7bcf7042953

--- javafx/src/main/java/org/jminor/javafx/framework/ui/EntityApplication.java
+++ javafx/src/main/java/org/jminor/javafx/framework/ui/EntityApplication.java
@@ -8,20 +8,30 @@ import org.jminor.common.db.exception.DatabaseException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public abstract class EntityApplication extends Application {
 
+  private static final String DEFAULT_ICON_FILE_NAME = "jminor_logo32.gif";
+
   private final String applicationTitle;
+  private final String iconFileName;
 
   public EntityApplication(final String applicationTitle) {
+    this(applicationTitle, DEFAULT_ICON_FILE_NAME);
+  }
+
+  public EntityApplication(final String applicationTitle, final String iconFileName) {
     this.applicationTitle = applicationTitle;
+    this.iconFileName = iconFileName;
     Thread.currentThread().setUncaughtExceptionHandler((thread, throwable) -> handleException(throwable));
   }
 
   @Override
   public final void start(final Stage stage) throws Exception {
     stage.setTitle(applicationTitle);
+    stage.getIcons().add(new Image(EntityApplication.class.getResourceAsStream(iconFileName)));
     stage.setScene(initializeApplicationScene(stage));
     stage.show();
   }
