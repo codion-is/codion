2eadbfc1e1edbc2e8c45763be5a6e9f175bc1731
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2016-05-16 14:10:53 +0000

    Build improvements
    git-svn-id: svn://svn.hafro.is/jminor/trunk@8166 51816742-9f83-410d-85ed-e7bcf7042953

--- demos/create_h2_db.sql
+++
@@ -1,6 +0,0 @@
-RUNSCRIPT FROM 'chinook/src/main/sql/create_schema.sql';
-RUNSCRIPT FROM 'empdept/src/main/sql/create_schema.sql';
-RUNSCRIPT FROM 'petstore/src/main/sql/create_schema.sql';
-RUNSCRIPT FROM 'world/src/main/sql/create_schema.sql';
-create user if not exists scott password 'tiger';
-alter user scott admin true;
