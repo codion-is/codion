699e593755019196381ab0590c6e2281a71ab477
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2017-01-11 09:59:05 +0000

    Copyright updated
    git-svn-id: svn://svn.hafro.is/jminor/trunk@8333 51816742-9f83-410d-85ed-e7bcf7042953

--- common/src/test/java/org/jminor/common/model/table/DefaultColumnSummaryModelTest.java
+++ common/src/test/java/org/jminor/common/model/table/DefaultColumnSummaryModelTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004 - 2016, Björn Darri Sigurðsson. All Rights Reserved.
+ * Copyright (c) 2004 - 2017, Björn Darri Sigurðsson. All Rights Reserved.
  */
 package org.jminor.common.model.table;
 
--- javafx/src/main/java/org/jminor/javafx/framework/model/FXConditionModelProvider.java
+++ javafx/src/main/java/org/jminor/javafx/framework/model/FXConditionModelProvider.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (c) 2004 - 2017, Björn Darri Sigurðsson. All Rights Reserved.
+ */
 package org.jminor.javafx.framework.model;
 
 import org.jminor.framework.db.EntityConnectionProvider;
