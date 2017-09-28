380b546ad01faa1dee67af13aee8ab672130db84
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2017-09-28 15:24:55 +0000

    LocalEntityConnections.createConnection() now returns LocalEntityConnection instead of EntityConnection
    git-svn-id: svn://svn.hafro.is/jminor/trunk@8595 51816742-9f83-410d-85ed-e7bcf7042953

--- framework/db-local/src/main/java/org/jminor/framework/db/local/LocalEntityConnections.java
+++ framework/db-local/src/main/java/org/jminor/framework/db/local/LocalEntityConnections.java
@@ -13,7 +13,7 @@ import org.jminor.framework.domain.Entities;
 import java.sql.Connection;
 
 /**
- * A factory class providing a local EntityConnection instances.
+ * A factory class providing a LocalEntityConnection instances.
  */
 public final class LocalEntityConnections {
 
@@ -24,10 +24,10 @@ public final class LocalEntityConnections {
    * @param entities the domain model entities
    * @param database the Database instance
    * @param user the user used for connecting to the database
-   * @return a new EntityConnection instance
+   * @return a new LocalEntityConnection instance
    * @throws DatabaseException in case there is a problem connecting to the database
    */
-  public static EntityConnection createConnection(final Entities entities, final Database database, final User user)
+  public static LocalEntityConnection createConnection(final Entities entities, final Database database, final User user)
           throws DatabaseException {
     return new LocalEntityConnection(entities, database, user, EntityConnection.USE_OPTIMISTIC_LOCKING.get(),
             EntityConnection.LIMIT_FOREIGN_KEY_FETCH_DEPTH.get(), EntityConnection.CONNECTION_VALIDITY_CHECK_TIMEOUT.get());
@@ -38,13 +38,12 @@ public final class LocalEntityConnections {
    * @param entities the domain model entities
    * @param database the Database instance
    * @param connection the connection object to base the entity connection on, it is assumed to be in a valid state
-   * @return a new EntityConnection instance, wrapping the given connection
+   * @return a new LocalEntityConnection instance, wrapping the given connection
    * @throws IllegalArgumentException in case the given connection is invalid or disconnected
-   * @throws DatabaseException in case a validation statement is required
-   * but could not be created
+   * @throws DatabaseException in case a validation statement is required but could not be created
    * @see org.jminor.common.db.Database#supportsIsValid()
    */
-  public static EntityConnection createConnection(final Entities entities, final Database database, final Connection connection)
+  public static LocalEntityConnection createConnection(final Entities entities, final Database database, final Connection connection)
           throws DatabaseException {
     return new LocalEntityConnection(entities, database, connection, EntityConnection.USE_OPTIMISTIC_LOCKING.get(),
             EntityConnection.LIMIT_FOREIGN_KEY_FETCH_DEPTH.get(), EntityConnection.CONNECTION_VALIDITY_CHECK_TIMEOUT.get());
@@ -52,7 +51,7 @@ public final class LocalEntityConnections {
 
   /**
    * @param entities the domain model entities
-   * @return A {@link MethodLogger} implementation tailored for EntityConnections
+   * @return A {@link MethodLogger} implementation tailored for LocalEntityConnections
    */
   public static MethodLogger createLogger(final Entities entities) {
     return new MethodLogger(EntityConnection.CONNECTION_LOG_SIZE.get(),
