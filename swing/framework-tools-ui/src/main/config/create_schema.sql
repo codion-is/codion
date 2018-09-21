cead1f2ffd84f5b03e2f2a8c3da159252132aaac
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2018-09-21 13:34:57 +0000

    Gradle application plugin configured for entity generator
    git-svn-id: svn://svn.hafro.is/jminor/trunk@8815 51816742-9f83-410d-85ed-e7bcf7042953

---
+++ swing/framework-tools-ui/src/main/config/create_schema.sql
@@ -0,0 +1,25 @@
+create user if not exists scott password 'tiger';
+alter user scott admin true;
+
+create schema scott;
+
+CREATE TABLE scott.dept (
+  deptno INT NOT NULL,
+  dname VARCHAR(14) NOT NULL,
+  loc VARCHAR(13),
+  constraint dept_pk primary key (deptno)
+);
+
+CREATE TABLE scott.emp (
+  empno INT NOT NULL,
+  ename VARCHAR(10) NOT NULL,
+  job VARCHAR(9),
+  mgr INT,
+  hiredate DATE,
+  sal DECIMAL(7, 2) NOT NULL,
+  comm DECIMAL(7, 2),
+  deptno INT NOT NULL,
+  constraint emp_pk primary key (empno),
+  constraint emp_dept_fk foreign key (deptno) references scott.dept(deptno),
+  constraint emp_mgr_fk foreign key (mgr) references scott.emp(empno)
+);
