99e5b1ed489f949c00042e304d07c4972e448e40
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-01-14 11:13:43 +0000

    Further dependency fiddling messup fixed
    git-svn-id: svn://svn.hafro.is/jminor/branches/java11@15757 51816742-9f83-410d-85ed-e7bcf7042953

--- framework/server/src/main/java/module-info.java
+++ framework/server/src/main/java/module-info.java
@@ -2,8 +2,8 @@ module org.jminor.framework.server {
   requires slf4j.api;
   requires java.management;
   requires jdk.management;
-  requires org.jminor.framework.db.local;
-  requires org.jminor.framework.db.remote;
+  requires transitive org.jminor.framework.db.local;
+  requires transitive org.jminor.framework.db.remote;
 
   exports org.jminor.framework.server;
 }
