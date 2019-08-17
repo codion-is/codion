05fa304fd9041f4987d247905609a1ac3b10c013
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-08-17 10:27:02 +0000

    Javadoc fix
    git-svn-id: svn://svn.hafro.is/jminor/branches/java11@17138 51816742-9f83-410d-85ed-e7bcf7042953

--- framework/db-core/src/main/java/org/jminor/framework/db/package-info.java
+++ framework/db-core/src/main/java/org/jminor/framework/db/package-info.java
@@ -1,9 +1,6 @@
 /**
  * Package configuration values:<br>
  * {@link org.jminor.framework.db.EntityConnectionProvider#CLIENT_DOMAIN_CLASS}<br>
- * {@link org.jminor.framework.db.EntityConnectionProvider#CLIENT_CONNECTION_TYPE}<br>
- * {@link org.jminor.framework.db.EntityConnectionProvider#HTTP_CONNECTION_PROVIDER}<br>
- * {@link org.jminor.framework.db.EntityConnectionProvider#REMOTE_CONNECTION_PROVIDER}<br>
- * {@link org.jminor.framework.db.EntityConnectionProvider#LOCAL_CONNECTION_PROVIDER}
+ * {@link org.jminor.framework.db.EntityConnectionProvider#CLIENT_CONNECTION_TYPE}
  */
 package org.jminor.framework.db;
