414d26903f58fd91b44d2186d1e16519d2f81839
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-10-14 16:15:31 +0000

    Documentation improvements
    git-svn-id: svn://svn.hafro.is/jminor/trunk@17655 51816742-9f83-410d-85ed-e7bcf7042953

--- demos/world/src/main/java/org/jminor/framework/demos/world/domain/World.java
+++ demos/world/src/main/java/org/jminor/framework/demos/world/domain/World.java
@@ -7,22 +7,19 @@ import org.jminor.framework.domain.Domain;
 import org.jminor.framework.domain.Entity;
 
 import java.sql.Types;
+import java.text.NumberFormat;
 
-import static org.jminor.framework.domain.Properties.columnProperty;
-import static org.jminor.framework.domain.Properties.foreignKeyProperty;
+import static org.jminor.common.Util.notNull;
+import static org.jminor.framework.domain.Properties.*;
 
+// tag::entityAndPropertyIds[]
 public final class World extends Domain {
-
-  public World() {
-    cityCountry();
-    countryLanguage();
-  }
 
   public static final String T_CITY = "world.city";
   public static final String CITY_ID = "id";
   public static final String CITY_NAME = "name";
   public static final String CITY_COUNTRYCODE = "countrycode";
-  public static final String CITY_COUNTRYCODE_FK = "countrycode_fk";
+  public static final String CITY_COUNTRY_FK = "country_fk";
   public static final String CITY_DISTRICT = "district";
   public static final String CITY_POPULATION = "population";
 
@@ -43,37 +40,85 @@ public final class World extends Domain {
   public static final String COUNTRY_CAPITAL = "capital";
   public static final String COUNTRY_CAPITAL_FK = "capital_fk";
   public static final String COUNTRY_CODE2 = "code2";
+  public static final String COUNTRY_CAPITAL_POPULATION = "capital_population";
 
-  void cityCountry() {
-    //disable this default check so we can define a cyclical foreign key between country and city
+  public static final String T_COUNTRYLANGUAGE = "world.countrylanguage";
+  public static final String COUNTRYLANGUAGE_COUNTRYCODE = "countrycode";
+  public static final String COUNTRYLANGUAGE_COUNTRY_FK = "country_fk";
+  public static final String COUNTRYLANGUAGE_LANGUAGE = "language";
+  public static final String COUNTRYLANGUAGE_ISOFFICIAL = "isofficial";
+  public static final String COUNTRYLANGUAGE_PERCENTAGE = "percentage";
+  public static final String COUNTRYLANGUAGE_NO_OF_SPEAKERS = "no_of_speakers";
+  // end::entityAndPropertyIds[]
+
+  public World() {
+    //disable this default check so we can define a foreign key relation
+    //from country to city without having defined the city entity
     Entity.Definition.STRICT_FOREIGN_KEYS.set(false);
 
+    city();
+    country();
+    countryLanguage();
+  }
+
+  // tag::defineCity[]
+  void city() {
+    define(T_CITY,
+            primaryKeyProperty(CITY_ID),
+            columnProperty(CITY_NAME, Types.VARCHAR, "Name")
+                    .setNullable(false)
+                    .setMaxLength(35),
+            foreignKeyProperty(CITY_COUNTRY_FK, "Country", T_COUNTRY,
+                    columnProperty(CITY_COUNTRYCODE, Types.VARCHAR))
+                    .setNullable(false),
+            columnProperty(CITY_DISTRICT, Types.VARCHAR, "District")
+                    .setNullable(false)
+                    .setMaxLength(20),
+            columnProperty(CITY_POPULATION, Types.INTEGER, "Population")
+                    .setNullable(false)
+                    .setFormat(NumberFormat.getIntegerInstance()))
+            .setKeyGenerator(sequenceKeyGenerator("world.city_seq"))
+            .setOrderBy(orderBy().ascending(CITY_NAME))
+            .setSearchPropertyIds(CITY_NAME)
+            .setStringProvider(new StringProvider(CITY_NAME))
+            .setCaption("City");
+  }
+  // end::defineCity[]
+
+  void country() {
     define(T_COUNTRY,
-            columnProperty(COUNTRY_CODE, Types.VARCHAR, "Country code")
-                    .setPrimaryKeyIndex(0)
+            // tag::primaryKey[]
+            primaryKeyProperty(COUNTRY_CODE, Types.VARCHAR, "Country code")
                     .setUpdatable(true)
                     .setMaxLength(3),
+            // end::primaryKey[]
             columnProperty(COUNTRY_NAME, Types.VARCHAR, "Name")
                     .setNullable(false)
                     .setMaxLength(52),
             columnProperty(COUNTRY_CONTINENT, Types.VARCHAR, "Continent")
                     .setNullable(false)
                     .setMaxLength(20),
+            // tag::columnProperty[]
             columnProperty(COUNTRY_REGION, Types.VARCHAR, "Region")
                     .setNullable(false),
             columnProperty(COUNTRY_SURFACEAREA, Types.DOUBLE, "Surface area")
                     .setNullable(false)
+                    .setFormat(NumberFormat.getNumberInstance())
                     .setMaximumFractionDigits(2),
             columnProperty(COUNTRY_INDEPYEAR, Types.INTEGER, "Indep. year")
-                    .setMin(-5000).setMax(5000),
+                    .setMin(-200).setMax(2500),
             columnProperty(COUNTRY_POPULATION, Types.INTEGER, "Population")
-                    .setNullable(false),
+                    .setNullable(false)
+                    .setFormat(NumberFormat.getIntegerInstance()),
             columnProperty(COUNTRY_LIFEEXPECTANCY, Types.DOUBLE, "Life expectancy")
                     .setMaximumFractionDigits(1)
                     .setMin(0).setMax(99),
+            // end::columnProperty[]
             columnProperty(COUNTRY_GNP, Types.DOUBLE, "GNP")
+                    .setFormat(NumberFormat.getNumberInstance())
                     .setMaximumFractionDigits(2),
             columnProperty(COUNTRY_GNPOLD, Types.DOUBLE, "GNP old")
+                    .setFormat(NumberFormat.getNumberInstance())
                     .setMaximumFractionDigits(2),
             columnProperty(COUNTRY_LOCALNAME, Types.VARCHAR, "Local name")
                     .setNullable(false)
@@ -81,8 +126,15 @@ public final class World extends Domain {
             columnProperty(COUNTRY_GOVERNMENTFORM, Types.VARCHAR, "Government form")
                     .setNullable(false),
             columnProperty(COUNTRY_HEADOFSTATE, Types.VARCHAR, "Head of state"),
+            // tag::foreignKeyPropertyCapital[]
             foreignKeyProperty(COUNTRY_CAPITAL_FK, "Capital", T_CITY,
                     columnProperty(COUNTRY_CAPITAL)),
+            // end::foreignKeyPropertyCapital[]
+            // tag::denormalizedViewProperty[]
+            denormalizedViewProperty(COUNTRY_CAPITAL_POPULATION, COUNTRY_CAPITAL_FK,
+                    getProperty(T_CITY, CITY_POPULATION), "Capital pop.")
+                    .setFormat(NumberFormat.getIntegerInstance()),
+            // end::denormalizedViewProperty[]
             columnProperty(COUNTRY_CODE2, Types.VARCHAR, "Code2")
                     .setNullable(false)
                     .setMaxLength(2))
@@ -90,38 +142,12 @@ public final class World extends Domain {
             .setSearchPropertyIds(COUNTRY_NAME)
             .setStringProvider(new StringProvider(COUNTRY_NAME))
             .setCaption("Country");
-
-    define(T_CITY,
-            columnProperty(CITY_ID)
-                    .setPrimaryKeyIndex(0),
-            columnProperty(CITY_NAME, Types.VARCHAR, "Name")
-                    .setNullable(false)
-                    .setMaxLength(35),
-            foreignKeyProperty(CITY_COUNTRYCODE_FK, "Country", T_COUNTRY,
-                    columnProperty(CITY_COUNTRYCODE, Types.VARCHAR))
-                    .setNullable(false),
-            columnProperty(CITY_DISTRICT, Types.VARCHAR, "District")
-                    .setNullable(false)
-                    .setMaxLength(20),
-            columnProperty(CITY_POPULATION, Types.INTEGER, "Population")
-                    .setNullable(false))
-            .setKeyGenerator(sequenceKeyGenerator("world.city_seq"))
-            .setOrderBy(orderBy().ascending(CITY_NAME))
-            .setSearchPropertyIds(CITY_NAME)
-            .setStringProvider(new StringProvider(CITY_NAME))
-            .setCaption("City");
   }
-
-  public static final String T_COUNTRYLANGUAGE = "world.countrylanguage";
-  public static final String COUNTRYLANGUAGE_COUNTRYCODE = "countrycode";
-  public static final String COUNTRYLANGUAGE_COUNTRYCODE_FK = "countrycode_fk";
-  public static final String COUNTRYLANGUAGE_LANGUAGE = "language";
-  public static final String COUNTRYLANGUAGE_ISOFFICIAL = "isofficial";
-  public static final String COUNTRYLANGUAGE_PERCENTAGE = "percentage";
 
   void countryLanguage() {
     define(T_COUNTRYLANGUAGE,
-            foreignKeyProperty(COUNTRYLANGUAGE_COUNTRYCODE_FK, "Country", T_COUNTRY,
+            // tag::compositePrimaryKey[]
+            foreignKeyProperty(COUNTRYLANGUAGE_COUNTRY_FK, "Country", T_COUNTRY,
                     columnProperty(COUNTRYLANGUAGE_COUNTRYCODE, Types.VARCHAR)
                             .setPrimaryKeyIndex(0)
                             .setUpdatable(true))
@@ -129,14 +155,29 @@ public final class World extends Domain {
             columnProperty(COUNTRYLANGUAGE_LANGUAGE, Types.VARCHAR, "Language")
                     .setPrimaryKeyIndex(1)
                     .setUpdatable(true),
+            // end::compositePrimaryKey[]
+            // tag::booleanProperty[]
             columnProperty(COUNTRYLANGUAGE_ISOFFICIAL, Types.BOOLEAN, "Is official")
                     .setColumnHasDefaultValue(true)
                     .setNullable(false),
+            // end::booleanProperty[]
             columnProperty(COUNTRYLANGUAGE_PERCENTAGE, Types.DOUBLE, "Percentage")
                     .setNullable(false)
                     .setMaximumFractionDigits(1)
-                    .setMin(0).setMax(100))
-            .setOrderBy(orderBy().ascending(COUNTRYLANGUAGE_LANGUAGE).descending(COUNTRYLANGUAGE_PERCENTAGE))
+                    .setMin(0).setMax(100),
+            // tag::derivedProperty[]
+            derivedProperty(COUNTRYLANGUAGE_NO_OF_SPEAKERS, Types.INTEGER, "No. of speakers", sourceValues -> {
+              final Double percentage = (Double) sourceValues.get(COUNTRYLANGUAGE_PERCENTAGE);
+              final Entity country = (Entity) sourceValues.get(COUNTRYLANGUAGE_COUNTRY_FK);
+              if (notNull(percentage, country) && !country.isValueNull(COUNTRY_POPULATION)) {
+                return country.getInteger(COUNTRY_POPULATION) * (percentage / 100);
+              }
+
+              return null;
+            }, COUNTRYLANGUAGE_COUNTRY_FK, COUNTRYLANGUAGE_PERCENTAGE)
+                    .setFormat(NumberFormat.getIntegerInstance())
+            // end::derivedProperty[]
+    ).setOrderBy(orderBy().ascending(COUNTRYLANGUAGE_LANGUAGE).descending(COUNTRYLANGUAGE_PERCENTAGE))
             .setCaption("Language");
   }
 }
