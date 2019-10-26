115beed0c9bba1a39facbbd373b2c216f9b742a8
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-10-26 12:40:10 +0000

    Serialization whitelist updated

--- framework/server/src/test/java/org/jminor/framework/server/DefaultEntityConnectionServerTest.java
+++ framework/server/src/test/java/org/jminor/framework/server/DefaultEntityConnectionServerTest.java
@@ -317,6 +317,7 @@ public class DefaultEntityConnectionServerTest {
     DefaultEntityConnectionServer.SERVER_LOGIN_PROXY_CLASSES.set("org.jminor.framework.server.TestLoginProxy");
     DefaultEntityConnectionServer.SERVER_CONNECTION_VALIDATOR_CLASSES.set("org.jminor.framework.server.TestConnectionValidator");
     DefaultEntityConnectionServer.SERVER_CLIENT_LOGGING_ENABLED.set(true);
+    DefaultEntityConnectionServer.SERIALIZATION_FILTER_WHITELIST.set("src/main/security/serialization-whitelist.txt");
     Server.AUXILIARY_SERVER_CLASS_NAMES.set(TestWebServer.class.getName());
     Server.RMI_SERVER_HOSTNAME.set("localhost");
     Server.TRUSTSTORE.set("src/main/security/jminor_truststore.jks");
