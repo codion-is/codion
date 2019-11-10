d41cf4b7615ffc45b2f43540b55edb5d77e586a8
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-11-10 14:46:21 +0000

    domain.property package exported

--- framework/domain/src/main/java/module-info.java
+++ framework/domain/src/main/java/module-info.java
@@ -4,6 +4,7 @@
 module org.jminor.framework.domain {
   requires transitive org.jminor.common.db;
 
+  exports org.jminor.framework.domain.property;
   exports org.jminor.framework.domain;
   exports org.jminor.framework.i18n;
 }
