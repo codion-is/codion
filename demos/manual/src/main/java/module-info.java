a112ea5a144f524ff6b983bb6a19d3b48fdbcfb9
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-11-13 22:53:19 +0000

    Manual demo module-info fixed

--- demos/manual/src/main/java/module-info.java
+++ demos/manual/src/main/java/module-info.java
@@ -1,5 +1,11 @@
 module org.jminor.framework.demos.empdept {
   requires java.desktop;
   requires org.jminor.common.core;
+  requires org.jminor.framework.db.remote;
   requires org.jminor.swing.common.ui;
+  requires org.jminor.swing.framework.model;
+  requires org.jminor.swing.framework.tools;
+  requires org.jminor.swing.framework.ui;
+  requires org.jminor.framework.db.test;
+  requires org.junit.jupiter.api;
 }
