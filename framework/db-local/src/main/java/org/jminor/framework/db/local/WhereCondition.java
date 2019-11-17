2d9681f6279fc202d56208d4ea6b5644602de9f7
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-11-17 10:53:58 +0000

    Condition.expand() removed, functionality moved to WhereCondition

--- framework/db-local/src/main/java/org/jminor/framework/db/local/WhereCondition.java
+++ framework/db-local/src/main/java/org/jminor/framework/db/local/WhereCondition.java
@@ -4,11 +4,18 @@
 package org.jminor.framework.db.local;
 
 import org.jminor.common.Conjunction;
+import org.jminor.common.db.ConditionType;
 import org.jminor.framework.db.condition.Condition;
+import org.jminor.framework.db.condition.Conditions;
 import org.jminor.framework.db.condition.EntityCondition;
+import org.jminor.framework.domain.Entities;
 import org.jminor.framework.domain.Entity;
 import org.jminor.framework.domain.property.ColumnProperty;
+import org.jminor.framework.domain.property.ForeignKeyProperty;
+import org.jminor.framework.domain.property.Property;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -56,8 +63,11 @@ final class WhereCondition {
     }
     if (condition instanceof Condition.PropertyCondition) {
       final Condition.PropertyCondition propertyCondition = (Condition.PropertyCondition) condition;
-
-      return propertyCondition.expand(definition.getProperty(propertyCondition.getPropertyId()));
+      final Property property = definition.getProperty(propertyCondition.getPropertyId());
+      if (property instanceof ForeignKeyProperty) {
+        return foreignKeyCondition((ForeignKeyProperty) property, propertyCondition.getConditionType(),
+                propertyCondition.getValues());
+      }
     }
 
     return condition;
@@ -103,5 +113,51 @@ final class WhereCondition {
       case OR: return " or ";
       default: throw new IllegalArgumentException("Unknown conjunction: " + conjunction);
     }
+  }private static Condition foreignKeyCondition(final ForeignKeyProperty foreignKeyProperty,
+                                                final ConditionType conditionType, final Collection values) {
+    final List<Entity.Key> keys = getKeys(values);
+    if (foreignKeyProperty.isCompositeKey()) {
+      return Conditions.createCompositeKeyCondition(foreignKeyProperty.getProperties(), conditionType, keys);
+    }
+
+    if (keys.size() == 1) {
+      final Entity.Key entityKey = keys.get(0);
+
+      return Conditions.propertyCondition(foreignKeyProperty.getProperties().get(0).getPropertyId(), conditionType,
+              entityKey == null ? null : entityKey.getFirstValue());
+    }
+
+    return Conditions.propertyCondition(foreignKeyProperty.getProperties().get(0).getPropertyId(), conditionType,
+            Entities.getValues(keys));
+  }
+
+  private static List<Entity.Key> getKeys(final Object value) {
+    final List<Entity.Key> keys = new ArrayList<>();
+    if (value instanceof Collection) {
+      if (((Collection) value).isEmpty()) {
+        keys.add(null);
+      }
+      else {
+        for (final Object object : (Collection) value) {
+          keys.add(getKey(object));
+        }
+      }
+    }
+    else {
+      keys.add(getKey(value));
+    }
+
+    return keys;
+  }
+
+  private static Entity.Key getKey(final Object value) {
+    if (value == null || value instanceof Entity.Key) {
+      return (Entity.Key) value;
+    }
+    else if (value instanceof Entity) {
+      return ((Entity) value).getKey();
+    }
+
+    throw new IllegalArgumentException("Foreign key condition uses only Entity or Entity.Key instances for values");
   }
 }
