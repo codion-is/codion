c6447836260885034ff8352cd4c01e456556d939
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-11-23 15:59:05 +0000

    Javadoc fix

--- common/model/src/main/java/org/jminor/common/model/table/FilteredTableModel.java
+++ common/model/src/main/java/org/jminor/common/model/table/FilteredTableModel.java
@@ -150,7 +150,7 @@ public interface FilteredTableModel<R, C, T> extends FilteredModel<R>, Refreshab
    * @param searchText the text to search by
    * @return the search result coordinate, null if nothing was found
    * @see #isRegularExpressionSearch()
-   * @see Predicate#include(Object)
+   * @see Predicate#test(Object)
    */
   RowColumn findNextItemCoordinate(final int fromIndex, final boolean forward, final String searchText);
 
@@ -162,7 +162,7 @@ public interface FilteredTableModel<R, C, T> extends FilteredModel<R>, Refreshab
    * @param forward if true then the search is forward (towards higher row indexes), backwards otherwise
    * @param condition the search condition
    * @return the search result coordinate, null if nothing was found
-   * @see Predicate#include(Object)
+   * @see Predicate#test(Object)
    */
   RowColumn findNextItemCoordinate(final int fromIndex, final boolean forward, final Predicate<Object> condition);
 
