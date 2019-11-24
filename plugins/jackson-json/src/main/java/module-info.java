f1cf03a547a217e3cd2b0163df453afb85600b3e
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-11-24 18:49:08 +0000

    jackson-json module-info.java fixed

--- plugins/jackson-json/src/main/java/module-info.java
+++ plugins/jackson-json/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.jminor.plugin.jackson.json {
   requires com.fasterxml.jackson.databind;
   requires org.jminor.framework.domain;
+  requires org.jminor.framework.db.core;
 
-  exports org.jminor.plugin.jackson.json;
+  exports org.jminor.plugin.jackson.json.domain;
+  exports org.jminor.plugin.jackson.json.db;
 }
