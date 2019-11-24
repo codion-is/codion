61a0567c07b645f637f0ecc699cda25ea09a59d3
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-11-24 17:46:59 +0000

    jackson-json module-info.java added

---
+++ plugins/jackson-json/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.jminor.plugin.jackson.json {
+  requires com.fasterxml.jackson.databind;
+  requires org.jminor.framework.domain;
+
+  exports org.jminor.plugin.jackson.json;
+}
--- plugins/json/src/main/java/module-info.java
+++ plugins/json/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module org.jminor.plugin.json {
   requires json;
-  requires org.jminor.framework.db.core;
+  requires org.jminor.framework.domain;
 
   exports org.jminor.plugin.json;
 }
