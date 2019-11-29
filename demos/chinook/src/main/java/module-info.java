e8136e4742bce5c2e9cbaac4027f0432fc97dec6
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-11-29 14:45:48 +0000

    Chinook demo module-info.java, imagepanel added

--- demos/chinook/src/main/java/module-info.java
+++ demos/chinook/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module org.jminor.framework.demos.chinook {
   requires org.jminor.swing.framework.ui;
   requires org.jminor.javafx.framework;
   requires org.jminor.plugin.jasperreports;
+  requires org.jminor.plugin.imagepanel;
   requires jasperreports;
 
   exports org.jminor.framework.demos.chinook.beans.ui
