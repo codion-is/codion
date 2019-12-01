5fae8a177beef1f59ce06d999f3121e356564bcc
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-12-01 17:36:01 +0000

    LocalEntityConnectionProvider refactored, LocalEntityConnectionHandler and proxying removed

--- framework/db-local/src/main/java/org/jminor/framework/db/local/package-info.java
+++ framework/db-local/src/main/java/org/jminor/framework/db/local/package-info.java
@@ -3,7 +3,6 @@
  * {@link org.jminor.framework.db.local.LocalEntityConnection#CONNECTION_LOG_SIZE}<br>
  * {@link org.jminor.framework.db.local.LocalEntityConnection#USE_OPTIMISTIC_LOCKING}<br>
  * {@link org.jminor.framework.db.local.LocalEntityConnection#LIMIT_FOREIGN_KEY_FETCH_DEPTH}<br>
- * {@link org.jminor.framework.db.local.LocalEntityConnectionProvider#SHUTDOWN_EMBEDDED_DB_ON_DISCONNECT}<br>
- * {@link org.jminor.framework.db.local.LocalEntityConnectionProvider#METHOD_LOGGER_ENABLED}
+ * {@link org.jminor.framework.db.local.LocalEntityConnectionProvider#SHUTDOWN_EMBEDDED_DB_ON_DISCONNECT}
  */
 package org.jminor.framework.db.local;
