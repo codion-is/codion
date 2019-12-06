4a406b4aec4b7e2e2eedeba1177b68e144a83c23
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-12-06 22:07:28 +0000

    MasterDetailPanel renamed to HierarchyPanel

---
+++ swing/common-ui/src/main/java/org/jminor/swing/common/ui/HierarchyPanel.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2004 - 2019, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package org.jminor.swing.common.ui;
+
+import java.util.List;
+
+/**
+ * A panel which is part of a navigable tree hierarchy
+ */
+public interface HierarchyPanel {
+
+  /**
+   * @return the parent panel, if one exists
+   */
+  HierarchyPanel getParentPanel();
+
+  /**
+   * @return the child panels, an empty list if no child panels exist
+   */
+  List<HierarchyPanel> getChildPanels();
+
+  /**
+   * @return a child panel, if one exists, which child panel exactly is up the implementation
+   */
+  HierarchyPanel getSelectedChildPanel();
+
+  /**
+   * Activates (and shows) the given child panel
+   * @param childPanel the child panel to activate and show
+   */
+  void setSelectedChildPanel(final HierarchyPanel childPanel);
+
+  /**
+   * @return the previous sibling panel, if one exists, with wrap around
+   */
+  HierarchyPanel getPreviousSiblingPanel();
+
+  /**
+   * @return the next sibling panel, if one exists, with wrap around
+   */
+  HierarchyPanel getNextSiblingPanel();
+
+  /**
+   * Activates this panel, showing it and preparing it for input
+   */
+  void activatePanel();
+}
