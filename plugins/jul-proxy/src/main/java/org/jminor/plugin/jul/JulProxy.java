3d9cdcf7124aed0e2ed0c3b3cb31353e1d4c8ac5
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-12-10 11:30:55 +0000

    Java.util.logging JulProxy plugin added

---
+++ plugins/jul-proxy/src/main/java/org/jminor/plugin/jul/JulProxy.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2004 - 2019, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package org.jminor.plugin.jul;
+
+import org.jminor.common.LoggerProxy;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import static java.util.Arrays.asList;
+
+/**
+ * A java.util.logging LoggerProxy implementation
+ */
+public final class JulProxy implements LoggerProxy {
+
+  /** {@inheritDoc} */
+  @Override
+  public Object getLogLevel() {
+    return LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME).getLevel();
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void setLogLevel(final Object logLevel) {
+    if (!(logLevel instanceof Level)) {
+      throw new IllegalArgumentException("logLevel should be of type " + Level.class.getName());
+    }
+    LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel((Level) logLevel);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public List getLogLevels() {
+    return asList(Level.ALL, Level.SEVERE, Level.WARNING, Level.INFO, Level.CONFIG, Level.FINE, Level.FINER, Level.FINEST, Level.OFF);
+  }
+}
