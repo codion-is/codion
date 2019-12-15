e6539f9bc5d7b9e5cfb4557b75ddc95207af05fc
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-12-15 13:24:38 +0000

    EntityEditPanel.createForeignKeyField() reintroduced

--- swing/framework-ui/src/main/java/org/jminor/swing/framework/ui/EntityEditPanel.java
+++ swing/framework-ui/src/main/java/org/jminor/swing/framework/ui/EntityEditPanel.java
@@ -13,6 +13,7 @@ import org.jminor.common.i18n.Messages;
 import org.jminor.common.state.State;
 import org.jminor.common.state.StateObserver;
 import org.jminor.common.state.States;
+import org.jminor.common.value.AbstractValue;
 import org.jminor.common.value.PropertyValue;
 import org.jminor.framework.db.EntityConnectionProvider;
 import org.jminor.framework.domain.Entity;
@@ -29,6 +30,7 @@ import org.jminor.swing.common.ui.DialogExceptionHandler;
 import org.jminor.swing.common.ui.TemporalInputPanel;
 import org.jminor.swing.common.ui.TextInputPanel;
 import org.jminor.swing.common.ui.UiUtil;
+import org.jminor.swing.common.ui.ValueLinks;
 import org.jminor.swing.common.ui.checkbox.TristateCheckBox;
 import org.jminor.swing.common.ui.combobox.MaximumMatch;
 import org.jminor.swing.common.ui.combobox.SteppedComboBox;
@@ -1969,6 +1971,33 @@ public abstract class EntityEditPanel extends JPanel implements DialogExceptionH
   }
 
   /**
+   * Creates an uneditable JTextField bound to the property identified by {@code propertyId}
+   * @param propertyId the ID of the property to bind
+   * @return an uneditable JTextField bound to the property
+   */
+  protected final JTextField createForeignKeyField(final String propertyId) {
+    return createForeignKeyField(editModel.getEntityDefinition().getForeignKeyProperty(propertyId));
+  }
+
+  /**
+   * Creates an uneditable JTextField bound to the given property
+   * @param foreignKeyProperty the foreign key property to bind
+   * @return an uneditable JTextField bound to the property
+   */
+  protected final JTextField createForeignKeyField(final ForeignKeyProperty foreignKeyProperty) {
+    requireNonNull(foreignKeyProperty, "foreignKeyProperty");
+    final JTextField textField = new JTextField();
+    textField.setEditable(false);
+    textField.setFocusable(false);
+    textField.setToolTipText(foreignKeyProperty.getDescription());
+    ValueLinks.textValueLink(textField, new ForeignKeyModelValue(editModel, foreignKeyProperty.getPropertyId()));
+
+    setComponent(foreignKeyProperty.getPropertyId(), textField);
+
+    return textField;
+  }
+
+  /**
    * Creates a JLabel with a caption from the property identified by {@code propertyId}
    * @param propertyId the ID of the property from which to retrieve the caption
    * @return a JLabel for the given property
@@ -2069,6 +2098,33 @@ public abstract class EntityEditPanel extends JPanel implements DialogExceptionH
     }
     else {
       requestFocus();
+    }
+  }
+
+  private static final class ForeignKeyModelValue extends AbstractValue<String> {
+
+    private final EntityEditModel editModel;
+    private final String foreignKeyPropertyId;
+
+    private ForeignKeyModelValue(final EntityEditModel editModel, final String foreignKeyPropertyId) {
+      this.editModel = editModel;
+      this.foreignKeyPropertyId = foreignKeyPropertyId;
+      editModel.addValueListener(foreignKeyPropertyId, valueChange -> fireChangeEvent(get()));
+    }
+
+    @Override
+    public void set(final String value) {/*read only*/}
+
+    @Override
+    public String get() {
+      final Entity value = editModel.getForeignKey(foreignKeyPropertyId);
+
+      return value == null ? "" : value.toString();
+    }
+
+    @Override
+    public boolean isNullable() {
+      return false;
     }
   }
 
