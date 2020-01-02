0eacc6871cf899f25e9d2394a1435cdc4e06ae82
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-01-02 14:24:53 +0000

    ConnectionPoolProvider javadoc improved ChinookLoginProxy now uses a ConnectionPool

--- common/db/src/main/java/org/jminor/common/db/pool/ConnectionPoolProvider.java
+++ common/db/src/main/java/org/jminor/common/db/pool/ConnectionPoolProvider.java
@@ -10,6 +10,8 @@ import org.jminor.common.db.exception.DatabaseException;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Provides connection pool implementations
  */
@@ -23,7 +25,14 @@ public interface ConnectionPoolProvider {
    */
   ConnectionPool createConnectionPool(User user, Database database) throws DatabaseException;
 
+  /**
+   * Returns the {@link ConnectionPoolProvider} implementation found by the {@link ServiceLoader}
+   * of the given type.
+   * @return a {@link ConnectionPoolProvider} implementation of the given type from the {@link ServiceLoader}.
+   * @throws IllegalStateException in case no such {@link ConnectionPoolProvider} implementation is available.
+   */
   static ConnectionPoolProvider getConnectionPoolProvider(final String classname) {
+    requireNonNull(classname, "classname");
     final ServiceLoader<ConnectionPoolProvider> loader = ServiceLoader.load(ConnectionPoolProvider.class);
     for (final ConnectionPoolProvider provider : loader) {
       if (provider.getClass().getName().equals(classname)) {
@@ -34,6 +43,11 @@ public interface ConnectionPoolProvider {
     throw new IllegalArgumentException("No connection pool provider of type: " + classname + " available");
   }
 
+  /**
+   * Returns the first {@link ConnectionPoolProvider} implementation found by the {@link ServiceLoader}.
+   * @return a {@link ConnectionPoolProvider} implementation from the {@link ServiceLoader}.
+   * @throws IllegalStateException in case no {@link ConnectionPoolProvider} implementation is available.
+   */
   static ConnectionPoolProvider getConnectionPoolProvider() {
     final ServiceLoader<ConnectionPoolProvider> loader = ServiceLoader.load(ConnectionPoolProvider.class);
     final Iterator<ConnectionPoolProvider> iterator = loader.iterator();
