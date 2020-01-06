7f3166464c900e4189a91a8b590265891ab04528
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-01-06 17:18:48 +0000

    petclinic module-info.java fixed

--- demos/petclinic/src/main/java/module-info.java
+++ demos/petclinic/src/main/java/module-info.java
@@ -3,6 +3,8 @@ module org.jminor.framework.demos.petclinic {
 
   exports org.jminor.framework.demos.petclinic.domain.impl
           to org.jminor.framework.db.local;
+  exports org.jminor.framework.demos.petclinic.model
+          to org.jminor.swing.framework.model;
   exports org.jminor.framework.demos.petclinic.ui
           to org.jminor.swing.framework.ui;
 }
