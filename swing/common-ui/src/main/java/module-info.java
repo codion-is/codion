484f23deeed8502ab821dfc0627b16364f4730a6
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-01-12 15:15:12 +0000

    module-info.java fixed

--- swing/common-ui/src/main/java/module-info.java
+++ swing/common-ui/src/main/java/module-info.java
@@ -6,11 +6,12 @@ module org.jminor.swing.common.ui {
   exports org.jminor.swing.common.ui.checkbox;
   exports org.jminor.swing.common.ui.combobox;
   exports org.jminor.swing.common.ui.control;
+  exports org.jminor.swing.common.ui.dialog;
   exports org.jminor.swing.common.ui.images;
-  exports org.jminor.swing.common.ui.input;
   exports org.jminor.swing.common.ui.layout;
   exports org.jminor.swing.common.ui.reports;
   exports org.jminor.swing.common.ui.table;
   exports org.jminor.swing.common.ui.textfield;
+  exports org.jminor.swing.common.ui.value;
   exports org.jminor.swing.common.ui.worker;
 }
