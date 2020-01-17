31d290e4850ea0e5cc57371e477773b956255953
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-01-17 15:59:33 +0000

    module-info fixes

--- demos/world/src/main/java/module-info.java
+++ demos/world/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.jminor.framework.demos.world {
   requires org.jminor.swing.framework.ui;
 
+  exports org.jminor.framework.demos.world.domain;
   exports org.jminor.framework.demos.world.ui
           to org.jminor.swing.framework.ui;
 }
