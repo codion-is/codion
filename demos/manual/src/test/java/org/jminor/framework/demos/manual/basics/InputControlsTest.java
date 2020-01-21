e439f94f7f0d1402060a9c001e2fb4a79c1231a4
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-01-21 16:53:09 +0000

    Documentation improvements

---
+++ demos/manual/src/test/java/org/jminor/framework/demos/manual/basics/InputControlsTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2004 - 2020, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package org.jminor.framework.demos.manual.basics;
+
+import org.junit.jupiter.api.Test;
+
+public final class InputControlsTest {
+
+  @Test
+  void test() {
+    InputControls.checkBox();
+    InputControls.nullableCheckBox();
+    InputControls.booleanComboBox();
+    InputControls.textField();
+    InputControls.textArea();
+    InputControls.integerField();
+    InputControls.longField();
+    InputControls.doubleField();
+    InputControls.bigDecimalField();
+    InputControls.localTime();
+    InputControls.localDate();
+    InputControls.localDateTime();
+    InputControls.selectionComboBox();
+  }
+}
