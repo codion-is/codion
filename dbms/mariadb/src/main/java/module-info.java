909e2f9fd3a482167c0a9cd725d754f10f31a1b5
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-03-13 14:28:42 +0000

    MariaDb module related fixes

---
+++ dbms/mariadb/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * @provides org.jminor.common.db.Database
+ */
+module org.jminor.dbms.mariadb {
+  requires transitive org.jminor.common.db;
+
+  exports org.jminor.dbms.mariadb;
+
+  provides org.jminor.common.db.DatabaseProvider
+          with org.jminor.dbms.mariadb.MariaDbDatabaseProvider;
+}
