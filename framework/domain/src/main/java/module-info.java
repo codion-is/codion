44fe768d929bb766fac51c763b584dac9b000ad1
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-03-22 16:41:30 +0000

    module-info.java fixed

--- framework/domain/src/main/java/module-info.java
+++ framework/domain/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module org.jminor.framework.domain {
   requires transitive org.jminor.common.db;
 
   exports org.jminor.framework.domain.entity;
-  exports org.jminor.framework.domain.exception;
+  exports org.jminor.framework.domain.entity.exception;
   exports org.jminor.framework.domain.property;
   exports org.jminor.framework.domain;
   exports org.jminor.framework.i18n;
