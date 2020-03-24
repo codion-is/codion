c94900c0355c92f1e1a9594efb977827ed1c166e
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-03-24 10:40:33 +0000

    Minor demo improvements

--- demos/world/src/main/java/org/jminor/framework/demos/world/ui/CountryTablePanel.java
+++ demos/world/src/main/java/org/jminor/framework/demos/world/ui/CountryTablePanel.java
@@ -12,19 +12,16 @@ import org.jminor.swing.framework.ui.EntityTablePanel;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
 
 public final class CountryTablePanel extends EntityTablePanel {
 
-  private final JFreeChart cityPieChart;
   private final ChartPanel cityChartPanel;
 
   public CountryTablePanel(CountryTableModel tableModel) {
     super(tableModel);
-    cityPieChart = ChartFactory.createPieChart("Cities", tableModel.getCityChartDataset());
-    cityChartPanel = new ChartPanel(cityPieChart);
-    getTable().setDoubleClickAction(Controls.control(this::displayCityPieChart,
-            "displayPieChart", tableModel.getSelectionModel().getSelectionEmptyObserver().getReversedObserver()));
+    cityChartPanel = new ChartPanel(ChartFactory.createPieChart("Cities", tableModel.getCityChartDataset()));
+    getTable().setDoubleClickAction(Controls.control(this::displayCityPieChart, "displayPieChart",
+            tableModel.getSelectionModel().getSelectionEmptyObserver().getReversedObserver()));
   }
 
   private void displayCityPieChart() throws DatabaseException {
