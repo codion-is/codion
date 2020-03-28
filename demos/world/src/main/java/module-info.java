dfe0dda296f9e9632fb80a5c54f4718d1eff1c0d
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-03-28 14:37:51 +0000

    module-info.java fix

--- demos/world/src/main/java/module-info.java
+++ demos/world/src/main/java/module-info.java
@@ -3,6 +3,8 @@ module org.jminor.framework.demos.world {
   requires jfreechart;
 
   exports org.jminor.framework.demos.world.domain;
+  exports org.jminor.framework.demos.world.model
+          to org.jminor.swing.framework.model;
   exports org.jminor.framework.demos.world.ui
           to org.jminor.swing.framework.ui;
 }
