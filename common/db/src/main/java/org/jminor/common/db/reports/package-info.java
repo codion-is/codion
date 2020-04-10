622e651ed002ba32096a2cf87aee6a54f316cf6b
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-04-10 19:29:22 +0000

    javadoc fix

--- common/db/src/main/java/org/jminor/common/db/reports/package-info.java
+++ common/db/src/main/java/org/jminor/common/db/reports/package-info.java
@@ -1,5 +1,6 @@
 /**
  * Package configuration values:<br>
  * {@link org.jminor.common.db.reports.ReportWrapper#REPORT_PATH}
+ * {@link org.jminor.common.db.reports.ReportWrapper#CACHE_REPORTS}
  */
 package org.jminor.common.db.reports;
