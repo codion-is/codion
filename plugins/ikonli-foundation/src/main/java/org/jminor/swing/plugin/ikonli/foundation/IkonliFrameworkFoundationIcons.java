fff66f810aa314da858f21ef36db276368eb182e
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-04-18 19:20:29 +0000

    identifying package added

--- plugins/ikonli-foundation/src/main/java/org/jminor/swing/plugin/ikonli/foundation/IkonliFrameworkFoundationIcons.java
+++ plugins/ikonli-foundation/src/main/java/org/jminor/swing/plugin/ikonli/foundation/IkonliFrameworkFoundationIcons.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2004 - 2020, Björn Darri Sigurðsson. All Rights Reserved.
  */
-package org.jminor.swing.plugin.ikonli;
+package org.jminor.swing.plugin.ikonli.foundation;
 
 import org.jminor.swing.framework.ui.icons.FrameworkIcons;
 
