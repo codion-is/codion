186523a90dd8a7de4e202af176aebf140f21336f
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-04-18 20:05:05 +0000

    Docs improved

---
+++ swing/common-ui/src/main/java/org/jminor/swing/common/ui/icons/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package configuration values:<br>
+ * {@link org.jminor.swing.common.ui.icons.Icons#ICONS_CLASSNAME}<br>
+ */
+package org.jminor.swing.common.ui.icons;
---
+++ swing/framework-ui/src/main/java/org/jminor/swing/framework/ui/icons/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package configuration values:<br>
+ * {@link org.jminor.swing.framework.ui.icons.FrameworkIcons#FRAMEWORK_ICONS_CLASSNAME}<br>
+ */
+package org.jminor.swing.framework.ui.icons;
