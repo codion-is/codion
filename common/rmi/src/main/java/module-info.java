fb35fe7c88653e2c5d1d718e02dc063b093526cf
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-04-24 13:51:33 +0000

    module-info.java added

---
+++ common/rmi/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * RMI client/server classes.
+ */
+module org.jminor.common.rmi {
+  requires org.slf4j;
+  requires transitive java.rmi;
+  requires transitive org.jminor.common.core;
+
+  exports org.jminor.common.rmi.client;
+  exports org.jminor.common.rmi.server;
+  exports org.jminor.common.rmi.server.exception;
+
+  uses org.jminor.common.rmi.server.AuxiliaryServer;
+}
