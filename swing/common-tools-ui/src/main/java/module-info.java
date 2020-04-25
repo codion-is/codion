e377a57780f01f82e5af465b1cc3e5e8b84661e9
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-04-25 21:49:04 +0000

    module-info.java fix

--- swing/common-tools-ui/src/main/java/module-info.java
+++ swing/common-tools-ui/src/main/java/module-info.java
@@ -3,5 +3,6 @@ module org.jminor.swing.common.tools.ui {
   requires transitive org.jminor.swing.common.tools;
   requires transitive org.jminor.swing.common.ui;
 
-  exports org.jminor.swing.common.tools.loadtest.ui;
+  exports org.jminor.swing.common.tools.ui.loadtest;
+  exports org.jminor.swing.common.tools.ui.randomizer;
 }
--- swing/common-tools/src/main/java/module-info.java
+++ swing/common-tools/src/main/java/module-info.java
@@ -6,4 +6,5 @@ module org.jminor.swing.common.tools {
   requires transitive org.jminor.common.model;
 
   exports org.jminor.swing.common.tools.loadtest;
+  exports org.jminor.swing.common.tools.randomizer;
 }
