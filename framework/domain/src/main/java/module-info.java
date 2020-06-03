95ae0c05e084ac7f8dae79bdda9977a53cd74c31
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2020-06-03 14:29:52 +0000

    module-info.java fixed

--- framework/domain/src/main/java/module-info.java
+++ framework/domain/src/main/java/module-info.java
@@ -8,7 +8,6 @@ module is.codion.framework.domain {
   exports is.codion.framework.domain.attribute;
   exports is.codion.framework.domain.entity;
   exports is.codion.framework.domain.entity.exception;
-  exports is.codion.framework.domain.identity;
   exports is.codion.framework.domain.property;
   exports is.codion.framework.domain;
   exports is.codion.framework.i18n;
