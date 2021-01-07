03c9cc2c42b35ebee74f6f65b957d2f18a8e7ad8
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-01-07 16:08:58 +0000

    org.json module now requires static

--- swing/framework-model/src/main/java/module-info.java
+++ swing/framework-model/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module is.codion.swing.framework.model {
   requires org.slf4j;
-  requires org.json;
+  requires static org.json;
   requires transitive is.codion.framework.model;
   requires transitive is.codion.swing.common.model;
 
