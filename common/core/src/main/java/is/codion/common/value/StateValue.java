89a51682e103549aae6447f202196a65b0cfa9e7
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-02-19 20:54:24 +0000

    StateValue now extends AbstractValue

--- common/core/src/main/java/is/codion/common/value/StateValue.java
+++ common/core/src/main/java/is/codion/common/value/StateValue.java
@@ -3,30 +3,21 @@
  */
 package is.codion.common.value;
 
-import is.codion.common.event.EventDataListener;
-import is.codion.common.event.EventListener;
 import is.codion.common.state.State;
-
-import java.util.Collection;
-import java.util.Objects;
-import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
 /**
  * A boolean value based on a State, null values are translated to 'false'
  */
-final class StateValue implements Value<Boolean>  {
+final class StateValue extends AbstractValue<Boolean>  {
 
   private final State state;
 
   StateValue(final State state) {
+    super(false);
     this.state = requireNonNull(state);
-  }
-
-  @Override
-  public void set(final Boolean value) {
-    state.set(value != null && value);
+    this.state.addListener(this::notifyValueChange);
   }
 
   @Override
@@ -35,68 +26,7 @@ final class StateValue implements Value<Boolean>  {
   }
 
   @Override
-  public Optional<Boolean> toOptional() {
-    return Optional.of(state.get());
-  }
-
-  @Override
-  public boolean isNull() {
-    return false;
-  }
-
-  @Override
-  public boolean isNotNull() {
-    return !isNull();
-  }
-
-  @Override
-  public boolean isNullable() {
-    return false;
-  }
-
-  @Override
-  public boolean is(final Boolean value) {
-    return Objects.equals(get(), value);
-  }
-
-  @Override
-  public void addListener(final EventListener listener) {
-    state.addListener(listener);
-  }
-
-  @Override
-  public void removeListener(final EventListener listener) {
-    state.removeListener(listener);
-  }
-
-  @Override
-  public void addDataListener(final EventDataListener<Boolean> listener) {
-    state.addDataListener(listener);
-  }
-
-  @Override
-  public void removeDataListener(final EventDataListener<Boolean> listener) {
-    state.removeDataListener(listener);
-  }
-
-  @Override
-  public void link(final Value<Boolean> originalValue) {
-    new ValueLink<>(this, originalValue);
-  }
-
-  @Override
-  public void link(final ValueObserver<Boolean> originalValueObserver) {
-    set(requireNonNull(originalValueObserver, "originalValueObserver").get());
-    originalValueObserver.addDataListener(this::set);
-  }
-
-  @Override
-  public void addValidator(final Validator<Boolean> validator) {
-    throw new UnsupportedOperationException("Validation not implemented for state values");
-  }
-
-  @Override
-  public Collection<Validator<Boolean>> getValidators() {
-    throw new UnsupportedOperationException("Validation not implemented for state values");
+  protected void setValue(final Boolean value) {
+    state.set(Boolean.TRUE.equals(value));
   }
 }
