8f8979e524cde53cff2093a4a0a5e1c90552b13d
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-05-21 17:00:03 +0000

    ForeignKeySearchFieldBuilder.upperCase() and lowerCase() added

--- swing/framework-ui/src/main/java/is/codion/swing/framework/ui/component/DefaultForeignKeySearchFieldBuilder.java
+++ swing/framework-ui/src/main/java/is/codion/swing/framework/ui/component/DefaultForeignKeySearchFieldBuilder.java
@@ -8,6 +8,7 @@ import is.codion.common.value.Value;
 import is.codion.framework.domain.entity.Entity;
 import is.codion.framework.domain.property.ForeignKeyProperty;
 import is.codion.framework.model.EntitySearchModel;
+import is.codion.swing.common.ui.textfield.TextFields;
 import is.codion.swing.framework.ui.EntitySearchField;
 
 import java.util.List;
@@ -22,6 +23,8 @@ final class DefaultForeignKeySearchFieldBuilder
 
   private final EntitySearchModel searchModel;
   private int columns;
+  private boolean upperCase;
+  private boolean lowerCase;
   private Function<EntitySearchModel, EntitySearchField.SelectionProvider> selectionProviderFactory;
 
   DefaultForeignKeySearchFieldBuilder(final ForeignKeyProperty attribute, final Value<Entity> value,
@@ -37,6 +40,20 @@ final class DefaultForeignKeySearchFieldBuilder
   }
 
   @Override
+  public ForeignKeySearchFieldBuilder upperCase() {
+    this.upperCase = true;
+    this.lowerCase = false;
+    return this;
+  }
+
+  @Override
+  public ForeignKeySearchFieldBuilder lowerCase() {
+    this.lowerCase = true;
+    this.upperCase = false;
+    return this;
+  }
+
+  @Override
   public ForeignKeySearchFieldBuilder selectionProviderFactory(final Function<EntitySearchModel,
           EntitySearchField.SelectionProvider> selectionProviderFactory) {
     this.selectionProviderFactory = requireNonNull(selectionProviderFactory);
@@ -47,6 +64,12 @@ final class DefaultForeignKeySearchFieldBuilder
   protected EntitySearchField buildComponent() {
     final EntitySearchField searchField = new EntitySearchField(searchModel);
     searchField.setColumns(columns);
+    if (upperCase) {
+      TextFields.upperCase(searchField);
+    }
+    if (lowerCase) {
+      TextFields.lowerCase(searchField);
+    }
     if (selectionProviderFactory != null) {
       searchField.setSelectionProvider(selectionProviderFactory.apply(searchField.getModel()));
     }
