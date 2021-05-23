64456268d383059f2a85ef8c3823d55f1aa6d147
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-05-23 11:19:00 +0000

    DialogsTest added

---
+++ swing/common-ui/src/test/java/is/codion/swing/common/ui/dialog/DialogsTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2004 - 2021, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package is.codion.swing.common.ui.dialog;
+
+import is.codion.common.event.Event;
+import is.codion.common.user.User;
+import is.codion.swing.common.ui.control.Control;
+import is.codion.swing.common.ui.control.Controls;
+import is.codion.swing.common.ui.icons.Icons;
+
+import org.junit.jupiter.api.Test;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public final class DialogsTest {
+
+  @Test
+  public void dialogBuilder() {
+    Dialogs.dialogBuilder()
+            .owner(new JLabel())
+            .component(new JLabel())
+            .title("title")
+            .icon(Icons.icons().filter())
+            .modal(false)
+            .resizable(false)
+            .enterAction(Control.control(() -> {}))
+            .onClosedAction(Control.control(() -> {}))
+            .closeEvent(Event.event())
+            .confirmCloseListener(state -> {})
+            .disposeOnEscape(false)
+            .build();
+  }
+
+  @Test
+  public void exceptionDialogBuilder() {
+    Dialogs.exceptionDialogBuilder()
+            .owner(new JLabel())
+            .title("title")
+            .icon(Icons.icons().filter())
+            .message("message");
+  }
+
+  @Test
+  public void fileSelectionDialogBuilder() {
+    Dialogs.fileSelectionDialogBuilder()
+            .owner(new JLabel())
+            .title("title")
+            .startDirectory(System.getProperty("user.home"))
+            .confirmOverwrite(true);
+  }
+
+  @Test
+  public void loginDialogBuilder() {
+    Dialogs.loginDialogBuilder()
+            .owner(new JLabel())
+            .title("title")
+            .icon(Icons.icons().filter())
+            .validator(user -> {})
+            .southComponent(new JLabel())
+            .defaultUser(User.user("scott"));
+  }
+
+  @Test
+  public void okCancelDialogBuilder() {
+    Dialogs.okCancelDialogBuilder()
+            .owner(new JLabel())
+            .title("title")
+            .icon(Icons.icons().filter())
+            .component(new JLabel())
+            .okAction(Control.control(() -> {}))
+            .cancelAction(Control.control(() -> {}))
+            .build();
+  }
+
+  @Test
+  public void progressDialogBuilder() {
+    Dialogs.progressDialogBuilder()
+            .owner(new JLabel())
+            .title("title")
+            .indeterminate(true)
+            .stringPainted(true)
+            .northPanel(new JPanel())
+            .westPanel(new JPanel())
+            .buttonControls(Controls.builder()
+                    .control(Control.builder().command(() -> {}))
+                    .build())
+            .build();
+  }
+}
