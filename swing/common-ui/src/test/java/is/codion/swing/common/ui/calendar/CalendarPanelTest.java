7550e833cda461a0c6074d8931a2ba2a750eb5e6
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-08-21 15:47:06 +0000

    CalendarPanel, keyboard navigation finished, refactoring + minor improvements, test improved

--- swing/common-ui/src/test/java/is/codion/swing/common/ui/calendar/CalendarPanelTest.java
+++ swing/common-ui/src/test/java/is/codion/swing/common/ui/calendar/CalendarPanelTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public final class CalendarPanelTest {
 
   @Test
-  void test() {
+  void events() {
     final AtomicInteger dateChangedCounter = new AtomicInteger();
     final AtomicInteger dateTimeChangedCounter = new AtomicInteger();
     final LocalDateTime startDate = LocalDateTime.of(2021, 8, 21, 16, 30);
@@ -51,5 +51,65 @@ public final class CalendarPanelTest {
 
     panel.removeDateListener(dataListener);
     panel.removeDateTimeListener(dateTimeListener);
+  }
+
+  @Test
+  void navigation() {
+    final LocalDateTime startDate = LocalDateTime.of(2021, 8, 21, 16, 30);
+    final CalendarPanel panel = new CalendarPanel(true);
+    panel.setDateTime(startDate);
+
+    panel.previousYear();
+    assertEquals(startDate.with(ChronoField.YEAR, 2020), panel.getDateTime());
+    panel.nextYear();
+    assertEquals(startDate, panel.getDateTime());
+
+    panel.previousMonth();
+    assertEquals(startDate.with(ChronoField.MONTH_OF_YEAR, 7), panel.getDateTime());
+    panel.nextMonth();
+    assertEquals(startDate, panel.getDateTime());
+
+    panel.previousWeek();
+    assertEquals(startDate.with(ChronoField.DAY_OF_MONTH, 14), panel.getDateTime());
+    panel.nextWeek();
+    assertEquals(startDate, panel.getDateTime());
+
+    panel.previousDay();
+    assertEquals(startDate.with(ChronoField.DAY_OF_MONTH, 20), panel.getDateTime());
+    panel.nextDay();
+    assertEquals(startDate, panel.getDateTime());
+
+    panel.previousHour();
+    assertEquals(startDate.with(ChronoField.HOUR_OF_DAY, 15), panel.getDateTime());
+    panel.nextHour();
+    assertEquals(startDate, panel.getDateTime());
+
+    panel.previousMinute();
+    assertEquals(startDate.with(ChronoField.MINUTE_OF_HOUR, 29), panel.getDateTime());
+    panel.nextMinute();
+    assertEquals(startDate, panel.getDateTime());
+
+    final LocalDateTime date = LocalDateTime.of(2021, 12, 31, 23, 59);
+    panel.setDateTime(date);
+
+    panel.nextHour();
+    assertEquals(date.withYear(2022).withMonth(1).withDayOfMonth(1).withHour(0).withMinute(59), panel.getDateTime());
+    panel.previousHour();
+    assertEquals(date, panel.getDateTime());
+
+    panel.nextMinute();
+    assertEquals(date.withYear(2022).withMonth(1).withDayOfMonth(1).withHour(0).withMinute(0), panel.getDateTime());
+    panel.previousMinute();
+    assertEquals(date, panel.getDateTime());
+  }
+
+  @Test
+  void dateOnly() {
+    final LocalDateTime startDate = LocalDateTime.of(2021, 8, 21, 16, 30);
+
+    final CalendarPanel panel = new CalendarPanel();
+    panel.setDateTime(startDate);
+
+    assertEquals(startDate.withHour(0).withMinute(0), panel.getDateTime());
   }
 }
