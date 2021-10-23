a9c510743aae2b007f74d74509d790290fe45577
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-10-23 14:32:51 +0000

    Unnecessary method removed

--- framework/domain/src/main/java/is/codion/framework/domain/entity/DefaultEntity.java
+++ framework/domain/src/main/java/is/codion/framework/domain/entity/DefaultEntity.java
@@ -121,7 +121,7 @@ final class DefaultEntity implements Entity, Serializable {
 
   @Override
   public boolean isModified() {
-    return isModifiedInternal(false);
+    return isModified(false);
   }
 
   @Override
@@ -728,8 +728,23 @@ final class DefaultEntity implements Entity, Serializable {
     }
   }
 
-  private boolean isModifiedInternal(final boolean overrideModifiesEntity) {
-    return originalValues != null && writablePropertiesModified(overrideModifiesEntity);
+  private boolean isModified(final boolean overrideModifiesEntity) {
+    if (originalValues != null) {
+      for (final Attribute<?> attribute : originalValues.keySet()) {
+        final Property<?> property = definition.getProperty(attribute);
+        if (property instanceof ColumnProperty) {
+          final ColumnProperty<?> columnProperty = (ColumnProperty<?>) property;
+          if (columnProperty.isInsertable() && columnProperty.isUpdatable()) {
+            return true;
+          }
+        }
+        if (property instanceof TransientProperty) {
+          return overrideModifiesEntity || ((TransientProperty<?>) property).isModifiesEntity();
+        }
+      }
+    }
+
+    return false;
   }
 
   private <T> void setOriginalValue(final Attribute<T> attribute, final T originalValue) {
@@ -762,7 +777,7 @@ final class DefaultEntity implements Entity, Serializable {
     stream.writeObject(definition.getDomainName());
     stream.writeObject(definition.getEntityType().getName());
     stream.writeInt(definition.getSerializationVersion());
-    final boolean isModified = isModifiedInternal(true);
+    final boolean isModified = isModified(true);
     stream.writeBoolean(isModified);
     final List<Property<?>> properties = definition.getProperties();
     for (int i = 0; i < properties.size(); i++) {
