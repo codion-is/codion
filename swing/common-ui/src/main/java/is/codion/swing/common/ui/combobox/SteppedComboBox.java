eba3fc426ef9aba7e4ab608d9ad6d7a41de4602d
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-10-31 21:25:14 +0000

    SteppedComboBox, now uses the same font as text fields

--- swing/common-ui/src/main/java/is/codion/swing/common/ui/combobox/SteppedComboBox.java
+++ swing/common-ui/src/main/java/is/codion/swing/common/ui/combobox/SteppedComboBox.java
@@ -4,6 +4,7 @@ import javax.swing.ComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JScrollBar;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 import javax.swing.plaf.basic.BasicComboPopup;
 import javax.swing.plaf.basic.ComboPopup;
 import javax.swing.plaf.metal.MetalComboBoxUI;
@@ -30,6 +31,7 @@ public class SteppedComboBox<T> extends JComboBox<T> {
   public SteppedComboBox(final ComboBoxModel<T> comboBoxModel) {
     super(Objects.requireNonNull(comboBoxModel, "comboBoxModel"));
     setUI(new SteppedComboBoxUI());
+    setFont(UIManager.getFont("TextField.font"));
   }
 
   /**
