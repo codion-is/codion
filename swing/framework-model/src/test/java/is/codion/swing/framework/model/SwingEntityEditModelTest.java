ae953b02db90874766b978824c51341869f35abb
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-11-06 18:56:52 +0000

    EntityComboBoxModel.getEntity(primaryKey) now returns Optional

--- swing/framework-model/src/test/java/is/codion/swing/framework/model/SwingEntityEditModelTest.java
+++ swing/framework-model/src/test/java/is/codion/swing/framework/model/SwingEntityEditModelTest.java
@@ -81,9 +81,9 @@ public class SwingEntityEditModelTest {
     employeeEditModel.getForeignKeyComboBoxModel(TestDomain.EMP_MGR_FK);
     employeeEditModel.refreshComboBoxModels();
     assertNotSame(employeeEditModel.getForeignKeyComboBoxModel(TestDomain.EMP_MGR_FK)
-            .getEntity(blake.getPrimaryKey()), blake);
+            .getEntity(blake.getPrimaryKey()).orElse(null), blake);
     employeeEditModel.replaceForeignKeyValues(singletonList(blake));
     assertSame(employeeEditModel.getForeignKeyComboBoxModel(TestDomain.EMP_MGR_FK)
-            .getEntity(blake.getPrimaryKey()), blake);
+            .getEntity(blake.getPrimaryKey()).orElse(null), blake);
   }
 }
