1c32e478619eb9ea1f4a1db1b9fd8eb6692235bb
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2021-12-18 22:31:41 +0000

    ChinookLoadTest bug fixed, did not link detail models

--- demos/chinook/src/main/java/is/codion/framework/demos/chinook/testing/ChinookLoadTest.java
+++ demos/chinook/src/main/java/is/codion/framework/demos/chinook/testing/ChinookLoadTest.java
@@ -6,6 +6,10 @@ package is.codion.framework.demos.chinook.testing;
 import is.codion.common.model.CancelException;
 import is.codion.common.user.User;
 import is.codion.framework.db.EntityConnectionProvider;
+import is.codion.framework.demos.chinook.domain.Chinook.Album;
+import is.codion.framework.demos.chinook.domain.Chinook.Artist;
+import is.codion.framework.demos.chinook.domain.Chinook.Customer;
+import is.codion.framework.demos.chinook.domain.Chinook.Invoice;
 import is.codion.framework.demos.chinook.domain.Chinook.Playlist;
 import is.codion.framework.demos.chinook.domain.Chinook.PlaylistTrack;
 import is.codion.framework.demos.chinook.model.ChinookApplicationModel;
@@ -45,6 +49,21 @@ public final class ChinookLoadTest extends EntityLoadTestModel<ChinookApplicatio
             EntityConnectionProvider.connectionProvider().setDomainClassName("is.codion.framework.demos.chinook.domain.impl.ChinookImpl")
                     .setClientTypeId(ChinookAppPanel.class.getName()).setUser(getUser()));
 
+    final SwingEntityModel customerModel = applicationModel.getEntityModel(Customer.TYPE);
+    final SwingEntityModel invoiceModel = customerModel.getDetailModel(Invoice.TYPE);
+    customerModel.addLinkedDetailModel(invoiceModel);
+
+    final SwingEntityModel artistModel = applicationModel.getEntityModel(Artist.TYPE);
+    final SwingEntityModel albumModel = artistModel.getDetailModel(Album.TYPE);
+    final SwingEntityModel trackModel = albumModel.getDetailModel(Track.TYPE);
+
+    artistModel.addLinkedDetailModel(albumModel);
+    albumModel.addLinkedDetailModel(trackModel);
+
+    final SwingEntityModel playlistModel = applicationModel.getEntityModel(Playlist.TYPE);
+    final SwingEntityModel playlistTrackModel = playlistModel.getDetailModel(PlaylistTrack.TYPE);
+    playlistModel.addLinkedDetailModel(playlistTrackModel);
+
     /* Add a Genre model used in the ViewGenre scenario */
     final SwingEntityModel genreModel = new SwingEntityModel(Genre.TYPE, applicationModel.getConnectionProvider());
     final SwingEntityModel genreTrackModel = new SwingEntityModel(Track.TYPE, applicationModel.getConnectionProvider());
@@ -52,9 +71,6 @@ public final class ChinookLoadTest extends EntityLoadTestModel<ChinookApplicatio
     genreModel.addLinkedDetailModel(genreTrackModel);
 
     applicationModel.addEntityModel(genreModel);
-
-    final SwingEntityModel playlistModel = applicationModel.getEntityModel(Playlist.TYPE);
-    playlistModel.addLinkedDetailModel(playlistModel.getDetailModel(PlaylistTrack.TYPE));
 
     return applicationModel;
   }
