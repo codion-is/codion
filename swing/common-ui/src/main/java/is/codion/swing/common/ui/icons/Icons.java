2777014c33155e4721688fa95cdfbd70ebdf483e
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-01-29 18:16:04 +0000

    Icons.ICON_SIZE configuration value added, EntityTablePanel no longer sets the toolbar button sizes to textfield square DefaultIcons.filter() and configure() overloaded with size parameter

--- swing/common-ui/src/main/java/is/codion/swing/common/ui/icons/Icons.java
+++ swing/common-ui/src/main/java/is/codion/swing/common/ui/icons/Icons.java
@@ -15,6 +15,15 @@ import java.util.ServiceLoader;
  */
 public interface Icons {
 
+  int DEFAULT_ICON_SIZE = 16;
+
+  /**
+   * The icon size, note that this will affect the size of buttons<br>
+   * Value type: Integer<br>
+   * Default value: 16
+   */
+  PropertyValue<Integer> ICON_SIZE = Configuration.integerValue("is.codion.swing.iconSize", DEFAULT_ICON_SIZE);
+
   PropertyValue<String> ICONS_CLASSNAME = Configuration.stringValue("is.codion.swing.iconsClassName", DefaultIcons.class.getName());
 
   /**
@@ -23,9 +32,21 @@ public interface Icons {
   ImageIcon filter();
 
   /**
+   * @param size the icon size
+   * @return icon for the 'filter' action.
+   */
+  ImageIcon filter(int size);
+
+  /**
    * @return icon for the 'configure' action.
    */
   ImageIcon configure();
+
+  /**
+   * @param size the icon size
+   * @return icon for the 'configure' action.
+   */
+  ImageIcon configure(int size);
 
   /**
    * @return icon for the codion logo
