4c66d40e873eceb1742f51c156d279f17e0eadfe
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-02-09 20:53:13 +0000

    ForeignKeyComboBoxBuilder removed

--- swing/framework-ui/src/main/java/is/codion/swing/framework/ui/component/DefaultForeignKeyComboBoxBuilder.java
+++ swing/framework-ui/src/main/java/is/codion/swing/framework/ui/component/DefaultForeignKeyComboBoxBuilder.java
@@ -4,12 +4,13 @@
 package is.codion.swing.framework.ui.component;
 
 import is.codion.framework.domain.entity.Entity;
+import is.codion.swing.common.ui.component.ComboBoxBuilder;
 import is.codion.swing.common.ui.component.DefaultComboBoxBuilder;
 import is.codion.swing.framework.model.SwingEntityComboBoxModel;
 import is.codion.swing.framework.ui.EntityComboBox;
 
-final class DefaultForeignKeyComboBoxBuilder extends DefaultComboBoxBuilder<Entity, EntityComboBox, ForeignKeyComboBoxBuilder>
-        implements ForeignKeyComboBoxBuilder {
+final class DefaultForeignKeyComboBoxBuilder<B extends ComboBoxBuilder<Entity, EntityComboBox, B>>
+        extends DefaultComboBoxBuilder<Entity, EntityComboBox, B> {
 
   DefaultForeignKeyComboBoxBuilder(final SwingEntityComboBoxModel comboBoxModel) {
     super(comboBoxModel, null);
