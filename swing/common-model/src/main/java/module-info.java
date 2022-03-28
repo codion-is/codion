95524a9f43c8bdd463cbc8f337e0823c42bec9f9
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-03-28 21:52:29 +0000

    module-info.java fixed

--- swing/common-model/src/main/java/module-info.java
+++ swing/common-model/src/main/java/module-info.java
@@ -2,9 +2,9 @@ module is.codion.swing.common.model {
   requires transitive java.desktop;
   requires transitive is.codion.common.model;
 
-  exports is.codion.swing.common.model.checkbox;
-  exports is.codion.swing.common.model.combobox;
-  exports is.codion.swing.common.model.table;
-  exports is.codion.swing.common.model.textfield;
+  exports is.codion.swing.common.model.component.checkbox;
+  exports is.codion.swing.common.model.component.combobox;
+  exports is.codion.swing.common.model.component.table;
+  exports is.codion.swing.common.model.component.textfield;
   exports is.codion.swing.common.model.worker;
 }
--- swing/common-ui/src/main/java/module-info.java
+++ swing/common-ui/src/main/java/module-info.java
@@ -3,18 +3,18 @@ module is.codion.swing.common.ui {
   requires transitive is.codion.swing.common.model;
 
   exports is.codion.swing.common.ui;
-  exports is.codion.swing.common.ui.calendar;
-  exports is.codion.swing.common.ui.checkbox;
-  exports is.codion.swing.common.ui.combobox;
   exports is.codion.swing.common.ui.component;
+  exports is.codion.swing.common.ui.component.calendar;
+  exports is.codion.swing.common.ui.component.checkbox;
+  exports is.codion.swing.common.ui.component.combobox;
+  exports is.codion.swing.common.ui.component.panel;
+  exports is.codion.swing.common.ui.component.slider;
+  exports is.codion.swing.common.ui.component.spinner;
+  exports is.codion.swing.common.ui.component.table;
+  exports is.codion.swing.common.ui.component.textfield;
   exports is.codion.swing.common.ui.control;
   exports is.codion.swing.common.ui.dialog;
-  exports is.codion.swing.common.ui.icons;
+  exports is.codion.swing.common.ui.icon;
   exports is.codion.swing.common.ui.laf;
   exports is.codion.swing.common.ui.layout;
-  exports is.codion.swing.common.ui.panel;
-  exports is.codion.swing.common.ui.slider;
-  exports is.codion.swing.common.ui.spinner;
-  exports is.codion.swing.common.ui.table;
-  exports is.codion.swing.common.ui.textfield;
 }
