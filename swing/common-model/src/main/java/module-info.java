c520d396e88c4502258d4b6f503b26d9420ca74f
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-04-18 16:38:14 +0000

    module-info.java fixed

--- swing/common-model/src/main/java/module-info.java
+++ swing/common-model/src/main/java/module-info.java
@@ -5,6 +5,6 @@ module is.codion.swing.common.model {
   exports is.codion.swing.common.model.component.checkbox;
   exports is.codion.swing.common.model.component.combobox;
   exports is.codion.swing.common.model.component.table;
-  exports is.codion.swing.common.model.component.textfield;
+  exports is.codion.swing.common.model.component.text;
   exports is.codion.swing.common.model.worker;
 }
