59fff0e0aebf3429f5b9b11871a8c9fdd483e4f5
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-05-23 16:50:08 +0200

    module-info.java fixed

--- plugins/jackson-json/src/main/java/module-info.java
+++ plugins/jackson-json/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module is.codion.plugin.jackson.json {
   requires transitive com.fasterxml.jackson.databind;
+  requires transitive com.fasterxml.jackson.datatype.jsr310;
   requires is.codion.framework.domain;
   requires is.codion.framework.db.core;
 
