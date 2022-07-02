1a234aff82229299c8f2ed05271aabed28c5b888
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-07-02 15:29:46 +0000

    Tests added

---
+++ common/db/src/test/java/is/codion/common/db/operation/DefaultFunctionTypeTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2004 - 2022, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package is.codion.common.db.operation;
+
+import is.codion.common.db.exception.DatabaseException;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class DefaultFunctionTypeTest {
+
+  @Test
+  void test() throws DatabaseException {
+    DefaultFunctionType<String, String, String> functionType = new DefaultFunctionType<>("test");
+    assertEquals(functionType, new DefaultFunctionType<>("test"));
+    assertEquals(functionType.getName(), functionType.toString());
+    functionType.execute("conn", (connection, argument) -> "", "hello");
+  }
+}
---
+++ common/db/src/test/java/is/codion/common/db/operation/DefaultProcedureTypeTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2004 - 2022, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package is.codion.common.db.operation;
+
+import is.codion.common.db.exception.DatabaseException;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class DefaultProcedureTypeTest {
+
+  @Test
+  void test() throws DatabaseException {
+    DefaultProcedureType<String, String> procedureType = new DefaultProcedureType<>("test");
+    assertEquals(procedureType, new DefaultProcedureType<>("test"));
+    assertEquals(procedureType.getName(), procedureType.toString());
+    procedureType.execute("conn", (connection, argument) -> {}, "hello");
+  }
+}
