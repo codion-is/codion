315f9d645bf7da6ff7c7787a488aee2f7ec0256e
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-07-16 11:14:47 +0000

    HttpEntityConnectionJdk updated

--- framework/db-http/src/main/java/is/codion/framework/db/http/HttpEntityConnectionJdk.java
+++ framework/db-http/src/main/java/is/codion/framework/db/http/HttpEntityConnectionJdk.java
@@ -399,7 +399,7 @@ final class HttpEntityConnectionJdk implements EntityConnection {
   }
 
   @Override
-  public Entity selectSingle(Key key) throws DatabaseException {
+  public Entity select(Key key) throws DatabaseException {
     return selectSingle(condition(key));
   }
 
