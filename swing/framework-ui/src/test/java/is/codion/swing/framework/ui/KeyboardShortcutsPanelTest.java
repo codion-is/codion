c28979bd5e107ddd2f8c0d8549aea6acf058aab7
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-07-20 13:04:06 +0000

    EntityApplicationPanel, added keyboard shortcut item to Help menu, KeyboardShortcutsPanel added

---
+++ swing/framework-ui/src/test/java/is/codion/swing/framework/ui/KeyboardShortcutsPanelTest.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022, Björn Darri Sigurðsson. All Rights Reserved.
+ */
+package is.codion.swing.framework.ui;
+
+import org.junit.jupiter.api.Test;
+
+public final class KeyboardShortcutsPanelTest {
+
+  @Test
+  void constructor() {
+    new KeyboardShortcutsPanel();
+  }
+}
