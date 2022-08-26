c3bbd17f5fefc923581f435d5627ba39da87d7e5
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-08-26 12:30:57 +0000

    Chinook demo, update button added to InvoiceLineEditPanel

--- demos/chinook/src/main/java/is/codion/framework/demos/chinook/ui/InvoiceLineEditPanel.java
+++ demos/chinook/src/main/java/is/codion/framework/demos/chinook/ui/InvoiceLineEditPanel.java
@@ -4,14 +4,18 @@
 package is.codion.framework.demos.chinook.ui;
 
 import is.codion.framework.demos.chinook.domain.Chinook.InvoiceLine;
+import is.codion.swing.common.ui.component.Components;
 import is.codion.swing.framework.model.SwingEntityEditModel;
 import is.codion.swing.framework.ui.EntityEditPanel;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JToolBar;
 import java.awt.BorderLayout;
 
 import static is.codion.swing.common.ui.layout.Layouts.borderLayout;
+import static is.codion.swing.common.ui.layout.Layouts.flexibleGridLayout;
 
 public final class InvoiceLineEditPanel extends EntityEditPanel {
 
@@ -35,9 +39,19 @@ public final class InvoiceLineEditPanel extends EntityEditPanel {
             .columns(2)
             .action(getControl(ControlCode.INSERT));
 
+    JToolBar updateToolBar = Components.toolBar()
+            .floatable(false)
+            .action(getControl(ControlCode.UPDATE))
+            .build();
+
+    JPanel eastPanel = Components.panel(flexibleGridLayout(1, 2))
+            .add(createInputPanel(new JLabel(" "), updateToolBar))
+            .add(createInputPanel(new JLabel(" "), tableSearchField))
+            .build();
+
     setLayout(borderLayout());
     addInputPanel(InvoiceLine.TRACK_FK, BorderLayout.WEST);
     addInputPanel(InvoiceLine.QUANTITY, BorderLayout.CENTER);
-    add(createInputPanel(new JLabel(" "), tableSearchField), BorderLayout.EAST);
+    add(eastPanel, BorderLayout.EAST);
   }
 }
