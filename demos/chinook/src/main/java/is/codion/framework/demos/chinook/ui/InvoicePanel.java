b74317343cd379ce9493236ef49aeb816135e864
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-09-10 14:09:13 +0000

    No need to update the invoice total manually

--- demos/chinook/src/main/java/is/codion/framework/demos/chinook/ui/InvoicePanel.java
+++ demos/chinook/src/main/java/is/codion/framework/demos/chinook/ui/InvoicePanel.java
@@ -3,6 +3,7 @@
  */
 package is.codion.framework.demos.chinook.ui;
 
+import is.codion.framework.demos.chinook.domain.Chinook.Invoice;
 import is.codion.swing.framework.model.SwingEntityModel;
 import is.codion.swing.framework.ui.EntityPanel;
 
@@ -10,6 +11,7 @@ public final class InvoicePanel extends EntityPanel {
 
   public InvoicePanel(SwingEntityModel invoiceModel, EntityPanel invoiceLinePanel) {
     super(invoiceModel, new InvoiceEditPanel(invoiceModel.editModel(), invoiceLinePanel));
+    tablePanel().excludeFromUpdateMenu(Invoice.TOTAL);
     setIncludeDetailTabPane(false);
     setShowDetailPanelControls(false);
     addDetailPanel(invoiceLinePanel);
