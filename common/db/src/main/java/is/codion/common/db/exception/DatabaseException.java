eea375ded4f7e18ebf6c696450bf1dd516acc540
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-12-08 14:48:58 +0000

    DatabaseException, add serialVersionUID based on the last class version, for some backwards compatibility

--- common/db/src/main/java/is/codion/common/db/exception/DatabaseException.java
+++ common/db/src/main/java/is/codion/common/db/exception/DatabaseException.java
@@ -10,6 +10,8 @@ import java.sql.SQLException;
  */
 public class DatabaseException extends Exception {
 
+  private static final long serialVersionUID = 1483224395022566393L;
+
   /**
    * The sql statement being run when this exception occurred, if any, transient
    * so that it's not available client side if running in a server/client environment
