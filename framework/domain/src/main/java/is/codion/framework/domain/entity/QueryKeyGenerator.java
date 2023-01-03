f6c9a4250d43f39cdd31c2e901d6e0e5319747ca
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-01-03 12:07:01 +0000

    QueryKeyGenerator bug fixed, did not override beforeInsert() and did nothing

--- framework/domain/src/main/java/is/codion/framework/domain/entity/QueryKeyGenerator.java
+++ framework/domain/src/main/java/is/codion/framework/domain/entity/QueryKeyGenerator.java
@@ -3,7 +3,12 @@
  */
 package is.codion.framework.domain.entity;
 
+import is.codion.common.db.connection.DatabaseConnection;
 import is.codion.common.db.database.Database;
+import is.codion.framework.domain.property.ColumnProperty;
+
+import java.sql.SQLException;
+import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
@@ -13,6 +18,15 @@ final class QueryKeyGenerator extends AbstractQueriedKeyGenerator {
 
   QueryKeyGenerator(String query) {
     this.query = requireNonNull(query, "query");
+  }
+
+  @Override
+  public void beforeInsert(Entity entity, List<ColumnProperty<?>> primaryKeyProperties,
+                           DatabaseConnection connection) throws SQLException {
+    ColumnProperty<?> primaryKeyProperty = primaryKeyProperties.get(0);
+    if (entity.isNull(primaryKeyProperty.attribute())) {
+      selectAndPut(entity, primaryKeyProperty, connection);
+    }
   }
 
   @Override
