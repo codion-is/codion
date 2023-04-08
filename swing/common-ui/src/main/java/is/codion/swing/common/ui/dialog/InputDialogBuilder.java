b623fb40857940aa75367f3cdde6bde96bc6b48e
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-04-08 10:23:25 +0000

    InputDialogBuilder.inputValidState() added EntityTablePanel.updateSelectedEntities() uses the above to disable OK button on null value for non-null properties

--- swing/common-ui/src/main/java/is/codion/swing/common/ui/dialog/InputDialogBuilder.java
+++ swing/common-ui/src/main/java/is/codion/swing/common/ui/dialog/InputDialogBuilder.java
@@ -3,6 +3,8 @@
  */
 package is.codion.swing.common.ui.dialog;
 
+import is.codion.common.state.StateObserver;
+
 import javax.swing.JComponent;
 
 /**
@@ -27,6 +29,13 @@ public interface InputDialogBuilder<T> {
    * @return this builder instance
    */
   InputDialogBuilder<T> caption(String caption);
+
+  /**
+   * A StateObserver indicating whethere the input is valid, this state controls the enabled state of the OK button.
+   * @param inputValidState a StateObserver indicating whether the input value is valid
+   * @return this builder instance
+   */
+  InputDialogBuilder<T> inputValidState(StateObserver inputValidState);
 
   /**
    * Shows the input dialog and returns the value if the user presses OK
