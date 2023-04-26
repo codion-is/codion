16281352d894685fa4b03afa8beaaab0e25dfe33
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-04-26 15:33:39 +0000

    Merge fix

--- swing/framework-ui/src/main/java/module-info.java
+++ swing/framework-ui/src/main/java/module-info.java
@@ -3,7 +3,7 @@
  */
 module is.codion.swing.framework.ui {
   requires org.slf4j;
-  requires org.kordamp.ikonli.core;
+  requires transitive org.kordamp.ikonli.core;
   requires org.kordamp.ikonli.swing;
   requires transitive is.codion.swing.framework.model;
   requires transitive is.codion.swing.common.ui;
