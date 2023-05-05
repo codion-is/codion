d0d3fec8ddc2d2aa971f05eabcc1b24bb48d58b1
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-05-05 21:11:14 +0200

    Merge fix

--- framework/model/src/main/java/module-info.java
+++ framework/model/src/main/java/module-info.java
@@ -3,6 +3,7 @@
  */
 module is.codion.framework.model {
   requires org.slf4j;
+  requires org.json;
   requires transitive is.codion.common.model;
   requires transitive is.codion.framework.db.core;
 
