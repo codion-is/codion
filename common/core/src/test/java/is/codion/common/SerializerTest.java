f80359a3ce56b953ba7a70907761c8225b52c92a
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-05-18 07:52:49 +0200

    Serializer.serializeToFile() and deserializeFromFile() removed

--- common/core/src/test/java/is/codion/common/SerializerTest.java
+++ common/core/src/test/java/is/codion/common/SerializerTest.java
@@ -5,11 +5,8 @@ package is.codion.common;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
-import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -20,20 +17,5 @@ public class SerializerTest {
     assertNull(Serializer.deserialize(new byte[0]));
     assertEquals(0, Serializer.serialize(null).length);
     assertEquals(Integer.valueOf(4), Serializer.deserialize(Serializer.serialize(4)));
-  }
-
-  @Test
-  void serialize() throws IOException, ClassNotFoundException {
-    List<Integer> ints = asList(1, 2, 3, 4);
-    File file = File.createTempFile("FileUtilTest.serialize", ".txt");
-    file.deleteOnExit();
-
-    Serializer.serializeToFile(ints, file);
-
-    List<Integer> readInts = Serializer.deserializeFromFile(file);
-
-    assertEquals(ints, readInts);
-
-    file.delete();
   }
 }
