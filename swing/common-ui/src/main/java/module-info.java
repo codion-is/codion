9e68be7ef831eb90d3a80c449c0f56a41725d704
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-06-21 11:16:37 +0000

    Merge fix

--- swing/common-ui/src/main/java/module-info.java
+++ swing/common-ui/src/main/java/module-info.java
@@ -9,16 +9,22 @@ module is.codion.swing.common.ui {
 
   exports is.codion.swing.common.ui;
   exports is.codion.swing.common.ui.component;
+  exports is.codion.swing.common.ui.component.builder;
   exports is.codion.swing.common.ui.component.button;
   exports is.codion.swing.common.ui.component.calendar;
   exports is.codion.swing.common.ui.component.combobox;
   exports is.codion.swing.common.ui.component.label;
   exports is.codion.swing.common.ui.component.list;
   exports is.codion.swing.common.ui.component.panel;
+  exports is.codion.swing.common.ui.component.progressbar;
+  exports is.codion.swing.common.ui.component.scrollpane;
   exports is.codion.swing.common.ui.component.slider;
   exports is.codion.swing.common.ui.component.spinner;
+  exports is.codion.swing.common.ui.component.splitpane;
+  exports is.codion.swing.common.ui.component.tabbedpane;
   exports is.codion.swing.common.ui.component.table;
   exports is.codion.swing.common.ui.component.text;
+  exports is.codion.swing.common.ui.component.value;
   exports is.codion.swing.common.ui.control;
   exports is.codion.swing.common.ui.dialog;
   exports is.codion.swing.common.ui.icon;
