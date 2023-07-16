da60b013b116f380c96a0361d8762f13a565040c
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-07-16 15:08:31 +0000

    Merge fix

--- swing/common-ui/src/main/java/module-info.java
+++ swing/common-ui/src/main/java/module-info.java
@@ -25,6 +25,7 @@ module is.codion.swing.common.ui {
   exports is.codion.swing.common.ui.component.table;
   exports is.codion.swing.common.ui.component.text;
   exports is.codion.swing.common.ui.component.value;
+  exports is.codion.swing.common.ui.border;
   exports is.codion.swing.common.ui.control;
   exports is.codion.swing.common.ui.dialog;
   exports is.codion.swing.common.ui.icon;
