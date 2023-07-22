c952e028d430bd80611192d96497d7ffeded0e83
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-07-22 16:02:21 +0000

    Merge fix

--- framework/domain-test/src/main/java/module-info.java
+++ framework/domain-test/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module is.codion.framework.domain.test {
   requires org.slf4j;
   requires org.junit.jupiter.api;
   requires transitive is.codion.framework.db.core;
+  requires is.codion.framework.db.local;
 
   exports is.codion.framework.domain.entity.test;
 }
