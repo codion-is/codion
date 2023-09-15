21dd4cd30cd70b0fdba4b2105ebfbb0f04b78bfc
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-09-15 13:16:34 +0200

    Merge fix

--- swing/common-model-tools/src/main/java/module-info.java
+++ swing/common-model-tools/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 /**
  * Common Swing model tools, such as:<br>
  * <br>
- * {@link is.codion.swing.common.model.tools.loadtest.LoadTest}<br>
+ * {@link is.codion.swing.common.model.tools.loadtest.LoadTestModel}<br>
  * {@link is.codion.swing.common.model.tools.loadtest.UsageScenario}<br>
  */
 module is.codion.swing.common.model.tools {
