ee96900cb661e0e2cc05cca4be0fa5654cdfe76b
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-09-24 13:52:48 +0200

    GPL license applied

--- common/rmi/src/main/java/is/codion/common/rmi/server/SerializationWhitelist.java
+++ common/rmi/src/main/java/is/codion/common/rmi/server/SerializationWhitelist.java
@@ -1,5 +1,20 @@
 /*
- * Copyright (c) 2018 - 2023, Björn Darri Sigurðsson. All Rights Reserved.
+ * This file is part of Codion.
+ *
+ * Codion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Codion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codion.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2018 - 2023, Björn Darri Sigurðsson.
  */
 package is.codion.common.rmi.server;
 
@@ -9,9 +24,9 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.ObjectInputFilter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputFilter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
--- common/rmi/src/test/java/is/codion/common/rmi/server/SerializationWhitelistTest.java
+++ common/rmi/src/test/java/is/codion/common/rmi/server/SerializationWhitelistTest.java
@@ -1,5 +1,20 @@
 /*
- * Copyright (c) 2019 - 2023, Björn Darri Sigurðsson. All Rights Reserved.
+ * This file is part of Codion.
+ *
+ * Codion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Codion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codion.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2019 - 2023, Björn Darri Sigurðsson.
  */
 package is.codion.common.rmi.server;
 
@@ -8,9 +23,9 @@ import is.codion.common.rmi.server.SerializationWhitelist.SerializationFilter;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ObjectInputFilter;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputFilter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
