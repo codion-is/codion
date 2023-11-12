6acacf4a91b23682daec25e3eb661f057b69c528
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-11-12 20:00:43 +0000

    Minor demo fix

--- demos/employees/src/main/java/is/codion/framework/demos/employees/domain/Employees.java
+++ demos/employees/src/main/java/is/codion/framework/demos/employees/domain/Employees.java
@@ -212,7 +212,7 @@ public final class Employees extends DefaultDomain {
                     .beanProperty("hiredate")
                     .localeDateTimePattern(LocaleDateTimePattern.builder()
                             .delimiterDash()
-                            .yearTwoDigits()
+                            .yearFourDigits()
                             .build()),
             Employee.DEPARTMENT_LOCATION.define()
                     .denormalized(Employee.DEPARTMENT_FK, Department.LOCATION)
