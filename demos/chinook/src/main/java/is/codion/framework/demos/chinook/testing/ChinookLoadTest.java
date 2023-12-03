9552a1170a6d47be064c3297d336603bffd8478b
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-12-03 13:44:46 +0000

    ChinookLoadTest, InsertDeleteInvoice scenario added

--- demos/chinook/src/main/java/is/codion/framework/demos/chinook/testing/ChinookLoadTest.java
+++ demos/chinook/src/main/java/is/codion/framework/demos/chinook/testing/ChinookLoadTest.java
@@ -8,6 +8,7 @@ import is.codion.framework.db.EntityConnectionProvider;
 import is.codion.framework.demos.chinook.domain.Chinook;
 import is.codion.framework.demos.chinook.model.ChinookAppModel;
 import is.codion.framework.demos.chinook.testing.scenarios.InsertDeleteAlbum;
+import is.codion.framework.demos.chinook.testing.scenarios.InsertDeleteInvoice;
 import is.codion.framework.demos.chinook.testing.scenarios.LogoutLogin;
 import is.codion.framework.demos.chinook.testing.scenarios.RaisePrices;
 import is.codion.framework.demos.chinook.testing.scenarios.RandomPlaylist;
@@ -18,8 +19,10 @@ import is.codion.framework.demos.chinook.testing.scenarios.ViewGenre;
 import is.codion.framework.demos.chinook.testing.scenarios.ViewInvoice;
 import is.codion.framework.demos.chinook.ui.ChinookAppPanel;
 import is.codion.swing.common.model.tools.loadtest.LoadTestModel;
+import is.codion.swing.common.model.tools.loadtest.UsageScenario;
 import is.codion.swing.common.ui.tools.loadtest.LoadTestPanel;
 
+import java.util.Collection;
 import java.util.function.Function;
 
 import static java.util.Arrays.asList;
@@ -29,12 +32,16 @@ public final class ChinookLoadTest {
   private static final User UNIT_TEST_USER =
           User.parse(System.getProperty("codion.test.user", "scott:tiger"));
 
+  private static final Collection<UsageScenario<EntityConnectionProvider>> SCENARIOS =
+          asList(new ViewGenre(), new ViewCustomerReport(), new ViewInvoice(), new ViewAlbum(), new UpdateTotals(),
+                  new InsertDeleteAlbum(), new LogoutLogin(), new RaisePrices(),new RandomPlaylist(), new InsertDeleteInvoice());
+
   public static void main(String[] args) {
-    LoadTestModel<EntityConnectionProvider> testModel = LoadTestModel.builder(new ConnectionProviderFactory(), EntityConnectionProvider::close)
-            .usageScenarios(asList(new ViewGenre(), new ViewCustomerReport(), new ViewInvoice(), new ViewAlbum(),
-                    new UpdateTotals(), new InsertDeleteAlbum(), new LogoutLogin(), new RaisePrices(), new RandomPlaylist()))
-            .user(UNIT_TEST_USER)
-            .build();
+    LoadTestModel<EntityConnectionProvider> testModel =
+            LoadTestModel.builder(new ConnectionProviderFactory(), EntityConnectionProvider::close)
+                    .usageScenarios(SCENARIOS)
+                    .user(UNIT_TEST_USER)
+                    .build();
     new LoadTestPanel<>(testModel).run();
   }
 
--- demos/chinook/src/main/java/is/codion/framework/demos/chinook/testing/scenarios/LoadTestUtil.java
+++ demos/chinook/src/main/java/is/codion/framework/demos/chinook/testing/scenarios/LoadTestUtil.java
@@ -9,6 +9,7 @@ final class LoadTestUtil {
 
   private static final int MAX_ARTIST_ID = 275;
   private static final int MAX_CUSTOMER_ID = 59;
+  private static final int MAX_TRACK_ID = 3503;
 
   static final Random RANDOM = new Random();
 
@@ -18,5 +19,9 @@ final class LoadTestUtil {
 
   static long randomCustomerId() {
     return RANDOM.nextInt(MAX_CUSTOMER_ID) + 1;
+  }
+
+  static long randomTrackId() {
+    return RANDOM.nextInt(MAX_TRACK_ID) + 1;
   }
 }
