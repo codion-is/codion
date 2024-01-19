073f12d2313bddbea67a10e25d191d40ddae59cc
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2024-01-19 11:22:47 +0000

    World demo, minor improvements

--- demos/world/src/main/java/is/codion/framework/demos/world/ui/CountryEditPanel.java
+++ demos/world/src/main/java/is/codion/framework/demos/world/ui/CountryEditPanel.java
@@ -24,9 +24,6 @@ final class CountryEditPanel extends EntityEditPanel {
 
   CountryEditPanel(SwingEntityEditModel editModel) {
     super(editModel);
-    defaults().foreignKeyComboBoxPreferredWidth().set(PREFERRED_COMBO_BOX_WIDTH);
-    defaults().itemComboBoxPreferredWidth().set(PREFERRED_COMBO_BOX_WIDTH);
-    defaults().comboBoxPreferredWidth().set(PREFERRED_COMBO_BOX_WIDTH);
   }
 
   @Override
@@ -41,8 +38,10 @@ final class CountryEditPanel extends EntityEditPanel {
             .upperCase(true);
     createTextField(Country.NAME);
     createTextField(Country.LOCALNAME);
-    createItemComboBox(Country.CONTINENT);
-    createComboBox(Country.REGION);
+    createItemComboBox(Country.CONTINENT)
+            .preferredWidth(PREFERRED_COMBO_BOX_WIDTH);
+    createComboBox(Country.REGION)
+            .preferredWidth(PREFERRED_COMBO_BOX_WIDTH);
     createTextField(Country.SURFACEAREA)
             .columns(5);
     createTextField(Country.INDEPYEAR)
@@ -56,10 +55,12 @@ final class CountryEditPanel extends EntityEditPanel {
     createTextField(Country.GNPOLD)
             .columns(6);
     createComboBox(Country.GOVERNMENTFORM)
+            .preferredWidth(PREFERRED_COMBO_BOX_WIDTH)
             .editable(true);
     createTextField(Country.HEADOFSTATE);
     //create a panel with a button for adding a new city
     createForeignKeyComboBoxPanel(Country.CAPITAL_FK, this::createCapitalEditPanel)
+            .comboBoxPreferredWidth(PREFERRED_COMBO_BOX_WIDTH)
             .add(true);
     //add a field displaying the avarage city population for the selected country
     CountryEditModel editModel = editModel();
