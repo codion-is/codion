9fc7f1e2cf68eeb22413703d2d7f3e83e973d1ea
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2024-03-22 09:25:59 +0000

    module-info.java fixed

--- demos/schemabrowser/src/main/java/module-info.java
+++ demos/schemabrowser/src/main/java/module-info.java
@@ -23,6 +23,7 @@ module is.codion.framework.demos.schemabrowser {
   requires is.codion.swing.common.ui.tools;
   requires is.codion.swing.framework.model.tools;
   requires is.codion.swing.framework.ui;
+  requires com.formdev.flatlaf.intellijthemes;
 
   exports is.codion.framework.demos.schemabrowser.client.ui
           to is.codion.swing.framework.ui;
