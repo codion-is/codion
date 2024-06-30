3d8bf5f9e51ab64bfbd8757f2709cdc6648157cb
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2024-06-30 19:26:44 +0000

    Server monitor package restructuring

---
+++ tools/monitor/ui/src/main/java/is/codion/tools/monitor/ui/DatabaseMonitorPanel.java
@@ -0,0 +1,94 @@
+/*
+ * This file is part of Codion.
+ *
+ * Codion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Codion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codion.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2009 - 2024, Björn Darri Sigurðsson.
+ */
+package is.codion.tools.monitor.ui;
+
+import is.codion.tools.monitor.model.DatabaseMonitor;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+import static is.codion.swing.common.ui.component.Components.*;
+import static is.codion.swing.common.ui.control.Control.commandControl;
+import static java.util.Objects.requireNonNull;
+import static javax.swing.BorderFactory.createEtchedBorder;
+import static javax.swing.BorderFactory.createTitledBorder;
+
+/**
+ * A DatabaseMonitorPanel
+ */
+public final class DatabaseMonitorPanel extends JPanel {
+
+	private static final int SPINNER_COLUMNS = 3;
+
+	private final DatabaseMonitor model;
+
+	private final JFreeChart queriesPerSecondChart = ChartFactory.createXYStepChart(null,
+					null, null, null, PlotOrientation.VERTICAL, true, true, false);
+
+	private final ChartPanel queriesPerSecondChartPanel = new ChartPanel(queriesPerSecondChart);
+
+	/**
+	 * Instantiates a new DatabaseMonitorPanel
+	 * @param model the DatabaseMonitor to base this panel on
+	 */
+	public DatabaseMonitorPanel(DatabaseMonitor model) {
+		this.model = requireNonNull(model);
+		this.queriesPerSecondChart.getXYPlot().setDataset(model.queriesPerSecondCollection());
+		ChartUtil.linkColors(this, queriesPerSecondChart);
+		initializeUI();
+	}
+
+	private void initializeUI() {
+		setLayout(new BorderLayout());
+		add(tabbedPane()
+						.tab("Connection Pools", new PoolMonitorPanel(model.connectionPoolMonitor()))
+						.tab("Performance", chartPanel())
+						.build(), BorderLayout.CENTER);
+	}
+
+	private JPanel chartPanel() {
+		JPanel chartConfig = flexibleGridLayoutPanel(1, 3)
+						.border(createTitledBorder("Charts"))
+						.add(new JLabel("Update interval (s)"))
+						.add(integerSpinner(model.updateInterval())
+										.minimum(1)
+										.columns(SPINNER_COLUMNS)
+										.editable(false)
+										.build())
+						.add(button(commandControl(model::clearStatistics))
+										.text("Clear")
+										.build())
+						.build();
+
+		queriesPerSecondChartPanel.setBorder(createEtchedBorder());
+
+		return borderLayoutPanel()
+						.centerComponent(queriesPerSecondChartPanel)
+						.southComponent(borderLayoutPanel()
+										.westComponent(chartConfig)
+										.build())
+						.build();
+	}
+}
---
+++ tools/monitor/ui/src/test/java/is/codion/tools/monitor/ui/EntityServerMonitorPanelTest.java
@@ -0,0 +1,100 @@
+/*
+ * This file is part of Codion.
+ *
+ * Codion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Codion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codion.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2010 - 2024, Björn Darri Sigurðsson.
+ */
+package is.codion.tools.monitor.ui;
+
+import is.codion.common.db.database.Database;
+import is.codion.common.rmi.client.Clients;
+import is.codion.common.rmi.server.Server;
+import is.codion.common.rmi.server.ServerConfiguration;
+import is.codion.common.user.User;
+import is.codion.framework.server.EntityServer;
+import is.codion.framework.server.EntityServerAdmin;
+import is.codion.framework.server.EntityServerConfiguration;
+import is.codion.tools.monitor.TestDomain;
+import is.codion.tools.monitor.model.ConnectionPoolMonitor;
+import is.codion.tools.monitor.model.DatabaseMonitor;
+import is.codion.tools.monitor.model.EntityServerMonitor;
+import is.codion.tools.monitor.model.ServerMonitor;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.rmi.registry.LocateRegistry;
+import java.util.Collections;
+
+public class EntityServerMonitorPanelTest {
+
+	private static final User UNIT_TEST_USER =
+					User.parse(System.getProperty("codion.test.user", "scott:tiger"));
+
+	private static final User ADMIN_USER = User.parse("scott:tiger");
+	private static Server<?, EntityServerAdmin> server;
+	private static EntityServerAdmin admin;
+
+	public static final EntityServerConfiguration CONFIGURATION = configure();
+
+	@BeforeAll
+	public static synchronized void setUp() throws Exception {
+		EntityServer.startServer(CONFIGURATION);
+		server = (Server<?, EntityServerAdmin>) LocateRegistry.getRegistry(Clients.SERVER_HOSTNAME.get(),
+						CONFIGURATION.registryPort()).lookup(CONFIGURATION.serverName());
+		admin = server.serverAdmin(ADMIN_USER);
+	}
+
+	@AfterAll
+	public static synchronized void tearDown() throws Exception {
+		admin.shutdown();
+		server = null;
+	}
+
+	@Test
+	void test() throws Exception {
+		EntityServerMonitorPanel panel = new EntityServerMonitorPanel(new EntityServerMonitor("localhost",
+						CONFIGURATION.registryPort(), CONFIGURATION.adminUser()));
+		ServerMonitor serverMonitor = panel.model().hostMonitors().iterator().next().serverMonitors().iterator().next();
+		serverMonitor.updateInterval().set(1);
+		DatabaseMonitor databaseMonitor = serverMonitor.databaseMonitor();
+		databaseMonitor.updateInterval().set(1);
+		ConnectionPoolMonitor poolMonitor = databaseMonitor.connectionPoolMonitor().connectionPoolInstanceMonitors().iterator().next();
+		poolMonitor.collectSnapshotStatistics().set(true);
+		poolMonitor.collectCheckOutTimes().set(true);
+		poolMonitor.updateInterval().set(1);
+
+		Thread.sleep(1000);
+		poolMonitor.resetStatistics();
+		poolMonitor.clearStatistics();
+		serverMonitor.shutdown();
+		Thread.sleep(500);
+	}
+
+	private static EntityServerConfiguration configure() {
+		Clients.SERVER_HOSTNAME.set("localhost");
+		ServerConfiguration.RMI_SERVER_HOSTNAME.set("localhost");
+
+		return EntityServerConfiguration.builder(3223, 3221)
+						.adminPort(3223)
+						.adminUser(User.parse("scott:tiger"))
+						.connectionPoolUsers(Collections.singletonList(UNIT_TEST_USER))
+						.domainClassNames(Collections.singletonList(TestDomain.class.getName()))
+						.database(Database.instance())
+						.sslEnabled(false)
+						.build();
+	}
+}
