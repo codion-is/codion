943ce5c83e790c738b41c558cddf38f0e3e196a8
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2025-01-13 12:37:13 +0000

    spotless, json and logback upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("org.sonarqube") version "6.0.1.5171"
     id("com.github.ben-manes.versions") version "0.51.0"
     id("com.vanniktech.dependency.graph.generator") version "0.8.0"
-    id("com.diffplug.spotless") version "6.25.0"
+    id("com.diffplug.spotless") version "7.0.1"
     id("org.gradlex.extra-java-module-info") version "1.9"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("io.github.f-cramer.jasperreports") version "0.0.3"
