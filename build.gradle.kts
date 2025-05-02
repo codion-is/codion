96cb018093df3d8988a14ce230d6c04523eb54dc
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2025-05-02 10:22:09 +0200

    Gradle + 3d party upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("org.sonarqube") version "6.1.0.5360"
     id("com.github.ben-manes.versions") version "0.52.0"
     id("com.vanniktech.dependency.graph.generator") version "0.8.0"
-    id("com.diffplug.spotless") version "7.0.2"
+    id("com.diffplug.spotless") version "7.0.3"
     id("org.gradlex.extra-java-module-info") version "1.12"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("io.github.f-cramer.jasperreports") version "0.0.4"
