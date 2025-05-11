f8844767468540a0ebe20b31300f1364f00c5be5
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2025-05-11 15:17:48 +0200

    Migrate to Sonatype Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ nexusPublishing {
     packageGroup = "is.codion"
     repositories {
         sonatype {
-            nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
-            snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
         }
     }
 }
