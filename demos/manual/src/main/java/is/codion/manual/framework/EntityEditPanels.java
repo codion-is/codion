86a4e50bd540e77f933fff4d62c8a7ddd2787426
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2025-08-09 19:00:56 +0000

    Minor doc fix

--- demos/manual/src/main/java/is/codion/manual/framework/EntityEditPanels.java
+++ demos/manual/src/main/java/is/codion/manual/framework/EntityEditPanels.java
@@ -36,6 +36,8 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import java.math.BigDecimal;
@@ -200,6 +202,17 @@ public final class EntityEditPanels {
 							createItemComboBox(Demo.ITEM_LIST)
 											.build();
 			// end::item[]
+		}
+
+		private void panelLabel() {
+			// tag::panelLabel[]
+			JLabel label = createLabel(Demo.TEXT)
+							.build();
+
+			JPanel inputPanel = createInputPanel(Demo.TEXT)
+							.label(new JLabel("Label"))
+							.build();
+			// end::panelLabel[]
 		}
 	}
 }
