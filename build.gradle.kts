76b6470d0fd5de0d5ed11ae6e3e0d39694a11c31
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2025-10-17 09:09:04 +0000

    Sonarqube plugin upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("org.sonarqube") version "6.3.1.5724"
+    id("org.sonarqube") version "7.0.0.6105"
     id("com.github.ben-manes.versions") version "0.53.0"
     id("com.vanniktech.dependency.graph.generator") version "0.8.0"
     id("com.diffplug.spotless") version "8.0.0"
