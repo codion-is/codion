8b5bc9b651270628f93cdeeed17fc10c15dcd50f
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2025-11-01 14:46:02 +0000

    Doc javadoc link handling simplified + docs additions

--- documentation/build.gradle.kts
+++ documentation/build.gradle.kts
@@ -77,9 +77,6 @@ tasks.asciidoctor {
             "experimental" to true, "reproducible" to true, "linkcss" to true, "tabsize" to 2,
             "diagram-cachedir" to project.layout.buildDirectory.dir("asciidoc/images/diagram-cache")
                 .get().asFile.absolutePath,
-            "opar" to "(",
-            "cpar" to ")",
-            "comma" to ",",
             "common-core" to "/is.codion.common.core",
             "common-db" to "/is.codion.common.db",
             "common-model" to "/is.codion.common.model",
