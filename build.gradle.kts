b72c91d36c3f33001555ba9b04a06ed3d902ffe2
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2026-05-17 11:28:14 +0000

    Gradle + Spotless upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.sonarqube") version "7.3.0.8198"
     id("com.github.ben-manes.versions") version "0.54.0"
     id("com.vanniktech.dependency.graph.generator") version "0.8.0"
-    id("com.diffplug.spotless") version "8.4.0"
+    id("com.diffplug.spotless") version "8.5.1"
     id("org.gradlex.extra-java-module-info") version "1.14"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("io.github.f-cramer.jasperreports") version "0.0.4"
